ec1995188df574d20b9cc4eb1519ff145ca50220
deylinqff <maycraftminecraft4@gmail.com>
2025-02-20 23:18:43 -0600

    Actualizar descargas-play1.js

--- plugins/descargas-play1.js
+++ plugins/descargas-play1.js
@@ -146,7 +146,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   }
 };
 
-handler.command = handler.help = ['play', 'play2', 'ytmp3', 'yta', 'ytmp4', 'ytv'];
+handler.command = handler.help = ['play', 'play', 'ytmp3', 'yta', 'ytmp4', 'ytv'];
 handler.tags = ['downloader'];
 handler.group = true;
 handler.register = true;
