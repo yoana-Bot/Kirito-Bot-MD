b73380abce197b0955adf96cc94681ef87d6a7d1
deylinqff <maycraftminecraft4@gmail.com>
2025-02-21 14:09:29 -0600

    Actualizar rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -37,18 +37,18 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 20)
 
   let regbot = `╔══════════════════╗\n`
-  regbot += `║  🚀 *REGISTRO COMPLETADO* 🚀  ║\n`
+  regbot += `║  👑 *REGISTRO COMPLETADO*  \n`
   regbot += `╚══════════════════╝\n\n`
   regbot += `╭───[ 📌 *Información* ]───╮\n`
   regbot += `│ 👤 *Nombre:* ${name}\n`
   regbot += `│ 🎂 *Edad:* ${age} años\n`
   regbot += `│ 🆔 *ID:* ${sn}\n`
-  regbot += `╰──────────────────────╯\n\n`
+  regbot += `╰────────────────────╯\n\n`
   regbot += `╭───[ 🎁 *Recompensas* ]───╮\n`
   regbot += `│ 💰 *Monedas:* 40\n`
   regbot += `│ ⭐ *Experiencia:* 300\n`
   regbot += `│ 🎟 *Tokens:* 20\n`
-  regbot += `╰──────────────────────╯\n\n`
+  regbot += `╰────────────────────╯\n\n`
   regbot += `✨ *¡Bienvenido al sistema!* ✨`
 
   await m.react('📩')
