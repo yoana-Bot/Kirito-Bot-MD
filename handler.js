fabb80e0dd23d431d72cc673dd8f94a24c6033ee
deylinqff <maycraftminecraft4@gmail.com>
2025-02-20 20:33:03 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -585,16 +585,16 @@ let user2 = m.pushName || 'Anónimo'
 let verifyaleatorio = ['registrar', 'reg', 'verificar', 'verify', 'register'].getRandom()
 
 const msg = {
-rowner: `『✦』El comando *${comando}* solo puede ser usado por los creadores del bot.`, 
-owner: `『✦』El comando *${comando}* solo puede ser usado por los desarrolladores del bot.`, 
-mods: `『✦』El comando *${comando}* solo puede ser usado por los moderadores del bot.`, 
-premium: `『✦』El comando *${comando}* solo puede ser usado por los usuarios premium.`, 
-group: `『✦』El comando *${comando}* solo puede ser usado en grupos.`,
-private: `『✦』El comando *${comando}* solo puede ser usado al chat privado del bot.`,
-admin: `『✦』El comando *${comando}* solo puede ser usado por los administradores del grupo.`, 
-botAdmin: `『✦』Para ejecutar el comando *${comando}* debo ser administrador del grupo.`,
-unreg: `『✦』El comando *${comando}* solo puede ser usado por los usuarios registrado, registrate usando:\n> » #${verifyaleatorio} ${user2}.${edadaleatoria}`,
-restrict: `『✦』Esta caracteristica está desactivada.`
+rowner: `👑 El comando *${comando}* solo puede ser usado por los creadores del bot.`, 
+owner: `👑 El comando *${comando}* solo puede ser usado por los desarrolladores del bot.`, 
+mods: `👑 El comando *${comando}* solo puede ser usado por los moderadores del bot.`, 
+premium: `👑 El comando *${comando}* solo puede ser usado por los usuarios premium.`, 
+group: `👑 El comando *${comando}* solo puede ser usado en grupos.`,
+private: `👑 El comando *${comando}* solo puede ser usado al chat privado del bot.`,
+admin: `👑 El comando *${comando}* solo puede ser usado por los administradores del grupo.`, 
+botAdmin: `👑 Para ejecutar el comando *${comando}* debo ser administrador del grupo.`,
+unreg: `👑 El comando *${comando}* solo puede ser usado por los usuarios registrado, registrate usando:\n> » #${verifyaleatorio} ${user2}.${edadaleatoria}`,
+restrict: `👑 Esta caracteristica está desactivada.`
 }[type];
 if (msg) return m.reply(msg).then(_ => m.react('✖️'))}
 
