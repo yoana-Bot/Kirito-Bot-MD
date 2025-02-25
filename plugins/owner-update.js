bc2ca36d33a594a3c4bcf8b82f40757b741fcb30
deylinqff <maycraftminecraft4@gmail.com>
2025-02-24 21:49:40 -0600

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,44 +1,41 @@
 import { execSync } from 'child_process'
 
 var handler = async (m, { conn, text }) => {
+  m.react('🚀') 
+  try {
+    const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
+    let messager = stdout.toString();
 
-m.react('🚀') 
-try {
+    if (messager.includes('⚡ Ya estoy actualizado.')) messager = '⚡ Ya estoy actualizado a la última versión.';
+    if (messager.includes('👑 Actualizando.')) messager = '⚡ Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString();
 
-const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
-let messager = stdout.toString()
+    conn.reply(m.chat, messager);
+  } catch { 
+    try {
+      const status = execSync('git status --porcelain');
 
-if (messager.includes('⚡ Ya estoy actualizado.')) messager = '⚡ Ya estoy actualizado a la última versión.'
+      if (status.length > 0) {
+        const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
+          if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('kiritoSession/') || line.includes('npm-debug.log')) {
+            return null;
+          }
+          return '*→ ' + line.slice(3) + '*';
+        }).filter(Boolean);
 
-if (messager.includes('👑 Actualizando.')) messager = '⚡ Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString()
-conn.reply(m.chat, messager, m, rcanal,)
-
-} catch { 
-try {
-
-const status = execSync('git status --porcelain')
-
-if (status.length > 0) {
-const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
-if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('kiritoSession/') || line.includes('npm-debug.log')) {
-return null
-}
-return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
-if (conflictedFiles.length > 0) {
-const errorMessage = `⚡ Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio, Para actualizar, reinstala el Bot o realiza las actualizaciones manualmente.\n\n✰ *ARCHIVOS EN CONFLICTO*\n\n${conflictedFiles.join('\n')}`
-await conn.reply(m.chat, errorMessage, m, rcanal,)
-}
-}
-} catch (error) {
-console.error(error)
-let errorMessage2 = '⚠️ Ocurrió un error inesperado.'
-if (error.message) {
-errorMessage2 += '\n⚠️ Mensaje de error: ' + error.message;
-}
-await conn.reply(m.chat, errorMessage2, m, rcanal,)
-}
-}
-
+        if (conflictedFiles.length > 0) {
+          const errorMessage = `⚡ Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio, Para actualizar, reinstala el Bot o realiza las actualizaciones manualmente.\n\n✰ *ARCHIVOS EN CONFLICTO*\n\n${conflictedFiles.join('\n')}`;
+          await conn.reply(m.chat, errorMessage);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      let errorMessage2 = '⚠️ Ocurrió un error inesperado.';
+      if (error.message) {
+        errorMessage2 += '\n⚠️ Mensaje de error: ' + error.message;
+      }
+      await conn.reply(m.chat, errorMessage2);
+    }
+  }
 }
 
 handler.help = ['update', 'actualizar']
