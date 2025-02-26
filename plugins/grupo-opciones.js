832b3d59236273d8dcb82535bb04c324f977c18d
deylinqff <maycraftminecraft4@gmail.com>
2025-02-26 14:06:54 -0600

    Actualizar grupo-opciones.js

--- plugins/grupo-opciones.js
+++ plugins/grupo-opciones.js
@@ -1,31 +1,26 @@
 const handler = async (m, { conn }) => {
   const { welcome, autolevelup, antiBot, antiBot2, autoAceptar, autoRechazar, autoresponder, modoadmin, reaction, nsfw, detect, antiLink, antiLink2, antitoxic, antiTraba, antifake } = global.db.data.chats[m.chat];
 
-  const estado = (valor) => valor ? ' *`Activado*`' : ' `*Desactivado*`';
+  const estado = (valor) => valor ? ' *`Activado`*' : ' *`Desactivado`*';
 
   const text = `*PANEL DE CONFIGURACIÓN DEL GRUPO* 
 
 
-> ⚔️ *Anti-Bots:* ${estado(antiBot)}  
-> ⚔️ *Anti-SubBots:* ${estado(antiBot2)}  
-> ⚔️ *Modo Admin:* ${estado(modoadmin)}  
-> 🔗 *Anti-Enlaces:* ${estado(antiLink)}  
-> 🔗 *Anti-Enlaces Avanzado:* ${estado(antiLink2)}  
-> 🛡 *Anti-Tóxicos:* ${estado(antitoxic)}  
-> ⚠️ *Anti-Trabas:* ${estado(antiTraba)}  
-> 👻 *Anti-Fakes:* ${estado(antifake)}  
-
-🌟 *Sistema de Poder y Nivel*  
-> 🔺 *Subida de Nivel Automática:* ${estado(autolevelup)}  
-> 🤖 *Respuesta Automática (IA):* ${estado(autoresponder)}  
-
-
-> ✨ *Bienvenida a Guerreros:* ${estado(welcome)}  
-> ✅ *Aceptación Automática de Reinos:* ${estado(autoAceptar)}  
-> ❌ *Rechazo Automático de Invasores:* ${estado(autoRechazar)}  
-> 👀 *Detección de Cambios:* ${estado(detect)}  
-> 😂 *Reacciones Épicas:* ${estado(reaction)}  
-> 🔞 *Modo +18:* ${estado(nsfw)}  
+⚔️ *Anti-Bots:* ${estado(antiBot)}  
+⚔️ *Anti-SubBots:* ${estado(antiBot2)}  
+⚔️ *Modo Admin:* ${estado(modoadmin)}  
+🔗 *Anti-Enlaces:* ${estado(antiLink)}  
+🔗 *Anti-Enlaces Avanzado:* ${estado(antiLink2)}  
+🛡 *Anti-Tóxicos:* ${estado(antitoxic)}  
+⚠️ *Anti-Trabas:* ${estado(antiTraba)}  
+👻 *Anti-Fakes:* ${estado(antifake)}  
+🤖 *Respuesta Automática (IA):* ${estado(autoresponder)}  
+✨ *Bienvenida:* ${estado(welcome)}  
+✅ *Aceptación Automática:* ${estado(autoAceptar)}  
+❌ *Rechazo Automático:* ${estado(autoRechazar)}  
+👀 *Detección de Cambios:* ${estado(detect)}  
+😂 *Reacciones Épicas:* ${estado(reaction)}  
+🔞 *Modo +18:* ${estado(nsfw)}  
 
 _*📝 Ejemplo de uso *#enable antilink*.*_`;
 
