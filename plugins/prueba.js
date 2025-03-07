30e04308a1ad1b347a0c2f7089e5c6a42f123b2e
deylinqff <maycraftminecraft4@gmail.com>
2025-03-07 14:30:51 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,17 +1,4 @@
 const handler = async (m, { conn }) => {
-  await conn.sendMessage(m.chat, { text: `¡Hola!
-
-Soy desarrollador web y estoy disponible para ayudarte a crear tu página web de forma profesional y personalizada. Si necesitas una web para tu negocio, emprendimiento o proyecto, puedo diseñarla según tus necesidades.
-
-💻 Desarrollo de páginas web – Desde $10, dependiendo de la complejidad y funciones requeridas.
-
-Si estás interesado, contáctame y trabajemos juntos en tu proyecto. ¡Estoy listo para ayudarte a crear tu sitio web!
-
-Contáctame: https://wa.link/jxjhek
-
-Visita mi plataforma: https://deylinqff.github.io/mundo-bot
-  ` }, { quoted: m });
-
   await conn.sendMessage(m.chat, { 
     text: 'Elige una opción:',
     buttons: [
