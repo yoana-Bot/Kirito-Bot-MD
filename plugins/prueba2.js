4100b9075bf29246085e1c67cb4d740e97ae9c76
deylinqff <maycraftminecraft4@gmail.com>
2025-03-07 17:00:32 -0600

    Crear prueba2.js

---
+++ plugins/prueba2.js
@@ -0,0 +1,40 @@
+const handler = async (m, { conn }) => {
+  await conn.sendMessage(m.chat, { text: `¡Hola!
+
+Soy desarrollador web y estoy disponible para ayudarte a crear tu página web de forma profesional y personalizada. Si necesitas una web para tu negocio, emprendimiento o proyecto, puedo diseñarla según tus necesidades.
+
+💻 Desarrollo de páginas web – Desde $10, dependiendo de la complejidad y funciones requeridas.
+
+Si estás interesado, contáctame y trabajemos juntos en tu proyecto. ¡Estoy listo para ayudarte a crear tu sitio web!
+
+Contáctame: https://wa.link/jxjhek
+
+Visita mi plataforma: https://deylinqff.github.io/mundo-bot
+  ` }, { quoted: m });
+
+  await conn.sendMessage(m.chat, { 
+    text: 'Elige una opción:',
+    buttons: [
+      {
+        buttonId: '.web servicios',
+        buttonText: { displayText: '🌐 Servicios' },
+      },
+      {
+        buttonId: '.web contacto',
+        buttonText: { displayText: '📞 Contacto' },
+      },
+      {
+        buttonId: '.web plataforma',
+        buttonText: { displayText: '💻 Plataforma' },
+      },
+    ],
+    footer: '¡Con gusto te ayudaré!',
+    viewOnce: true,
+  }, { quoted: m });
+};
+
+handler.tags = ['tools'];
+handler.help = ['webinfo'];
+handler.command = ['web', 'servicios', 'info'];
+
+export default handler;
