d16db697b1c98b9202b8ba8400ed2aa4d47d0953
deylinqff <maycraftminecraft4@gmail.com>
2025-03-07 17:50:39 -0600

    Actualizar _plataforma.js

--- plugins/_plataforma.js
+++ plugins/_plataforma.js
@@ -1,6 +1,6 @@
 const handler = async (m, { command }) => {
-  if (command === 'code') {
-    m.reply('Uso correcto del comando: .serbot --code');
+  if (command === 'platafor') {
+    m.reply('*plataforma*\n\n https://deylinqff.github.io/mundo-bot');
   }
 };
 
