fd6d681e8c666a71c954d13ec64c35708bda6979
deylinqff <maycraftminecraft4@gmail.com>
2025-03-07 13:57:50 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,27 +1,29 @@
-const handler = async (m, { command }) => {
+const handler = async (m, { command, conn }) => {
   if (command === 'code') {
-    m.reply('Uso correcto del comando:\n\n *.serbot --code*');
-  }
-      buttons: [
-        {
-          buttonId: '.imgg gato',
-          buttonText: { displayText: '😻 Gato' },
-        },
-        {
-          buttonId: '.imgg perro',
-          buttonText: { displayText: '🐶 Perro' },
-        },
-        {
-          buttonId: '.imgg raton',
-          buttonText: { displayText: '🐁 Rata' },
-        },
-        {
-          buttonId: '.imgg caballo',
-          buttonText: { displayText: '🐎 Caballo' },
-        },
-      ],
-};
+    const texto = 'Uso correcto del comando: .serbot --code';
 
+    const buttons = [
+      {
+        buttonId: '.imgg gato',
+        buttonText: { displayText: '😻 Gato' },
+      },
+      {
+        buttonId: '.imgg perro',
+        buttonText: { displayText: '🐶 Perro' },
+      },
+      {
+        buttonId: '.imgg raton',
+        buttonText: { displayText: '🐁 Rata' },
+      },
+      {
+        buttonId: '.imgg caballo',
+        buttonText: { displayText: '🐎 Caballo' },
+      }
+    ];
+
+    await conn.sendMessage(m.chat, { text: texto, buttons, footer: 'Código creado por Deyin' }, { quoted: m });
+  }
+};
 
 handler.help = ['serbot', 'serbot code'];
 handler.tags = ['serbot'];
