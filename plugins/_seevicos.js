eed1f0b92d78841e0465d7f0b741a673dfc66378
deylinqff <maycraftminecraft4@gmail.com>
2025-03-07 17:42:53 -0600

    Actualizar _seevicos.js

--- plugins/_seevicos.js
+++ plugins/_seevicos.js
@@ -1,6 +1,39 @@
 const handler = async (m, { command }) => {
   if (command === 'code') {
-    m.reply('Uso correcto del comando: .serbot --code');
+    m.reply(`*Servicios de un Creador de Página Web:*
+
+1. Diseño y Desarrollo Web: Creación de sitios web modernos y responsivos.
+
+2. Optimización SEO: Mejora de la visibilidad en motores de búsqueda.
+
+3. E-commerce: Desarrollo de tiendas online y sistemas de pago.
+
+4. Mantenimiento: Actualización y mantenimiento de seguridad.
+
+5. Rediseño: Modernización de páginas web antiguas.
+
+
+
+*Servicios de un Bot de WhatsApp:*
+
+1. Automatización de Respuestas: Respuestas automáticas y menú interactivo.
+
+2. Atención al Cliente 24/7: Soporte instantáneo y escalable.
+
+3. Notificaciones: Envío automático de mensajes, recordatorios y promociones.
+
+4. Integración con Pagos: Realizar pagos y compras directamente en WhatsApp.
+
+5. Encuestas: Recopilación de feedback a través de mensajes interactivos.
+
+
+*Servicios Combinados:*
+
+Integración Web-Bot: Conectar la página web con el bot de WhatsApp.
+
+Soporte Multicanal: Atención en web y WhatsApp.
+
+Automatización de Marketing: Campañas automáticas y seguimiento de usuarios.`);
   }
 };
 
