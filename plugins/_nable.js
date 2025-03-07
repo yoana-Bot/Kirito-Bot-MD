90dc1b24539fec54f0b0cf0c2723724f4b4de0de
deylinqff <maycraftminecraft4@gmail.com>
2025-03-06 22:13:08 -0600

    Actualizar _nable.js

--- plugins/_nable.js
+++ plugins/_nable.js
@@ -456,10 +456,10 @@ throw false
 global.opts['swonly'] = isEnable
 break
 default:
-if (!/[01]/.test(command)) return await conn.reply(m.chat, miniopcion,
+if (!/[01]/.test(command)) return await conn.reply(m.chat, miniopcion, m, rcanal);
 throw false;
 }
-conn.reply(m.chat, `〘⌬〙  *La Función ${type} Se Ha ${isEnable ? 'Activado' : 'Desactivado'} En Este ${isAll ? 'Bot' : isUser ? '' : 'Chat.'}*`);
+conn.reply(m.chat, `〘⌬〙  *La Función ${type} Se Ha ${isEnable ? 'Activado' : 'Desactivado'} En Este ${isAll ? 'Bot' : isUser ? '' : 'Chat.'}*`, m, rcanal, )
 
 }
 handler.help = ['en', 'dis'].map((v) => v + 'able <option>');
