ba57acd03fdfc3776ad830040fed3336291b8bff
deylinqff <maycraftminecraft4@gmail.com>
2025-03-07 09:52:27 -0600

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
 try {
-let res = await fetch('https://api.github.com/repos/The-King-Destroy/Yuki_Suou-Bot')
+let res = await fetch('https://deylinqff.github.io/mundo-bot')
 
 if (!res.ok) throw new Error('Error al obtener datos del repositorio')
 let json = await res.json()
