80b39eda569e72b1a96cf599005a097550090ee6
deylinqff <maycraftminecraft4@gmail.com>
2025-03-07 13:54:33 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -3,6 +3,24 @@ const handler = async (m, { command }) => {
     m.reply('Uso correcto del comando:\n\n *.serbot --code*');
   }
 };
+      buttons: [
+        {
+          buttonId: '.imgg gato',
+          buttonText: { displayText: '😻 Gato' },
+        },
+        {
+          buttonId: '.imgg perro',
+          buttonText: { displayText: '🐶 Perro' },
+        },
+        {
+          buttonId: '.imgg raton',
+          buttonText: { displayText: '🐁 Rata' },
+        },
+        {
+          buttonId: '.imgg caballo',
+          buttonText: { displayText: '🐎 Caballo' },
+        },
+      ],
 
 handler.help = ['serbot', 'serbot code'];
 handler.tags = ['serbot'];
