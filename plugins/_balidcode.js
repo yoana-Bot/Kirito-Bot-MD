32c2ec3946b2bba2033bbe049d5c19f4629a80e8
deylinqff <maycraftminecraft4@gmail.com>
2025-03-08 11:25:47 -0600

    Crear _balidcode.js

---
+++ plugins/_balidcode.js
@@ -0,0 +1,23 @@
+const handler = async (m, { conn }) => {
+  await conn.sendMessage(m.chat, { 
+    text: 'Uso correcto del comando:',
+    buttons: [
+      {
+        buttonId: 'servicios',
+        buttonText: { displayText: '#serbot --code' },
+      },
+      {
+        buttonId: 'contacto',
+        buttonText: { displayText: '#serbot' },
+      },
+       ],
+    footer: '¡KIRITO-BOT-MD!',
+    viewOnce: true,
+  }, { quoted: m });
+};
+
+handler.tags = ['tools'];
+handler.help = ['webinfo'];
+handler.command = ['code','Code'];
+
+export default handler;
