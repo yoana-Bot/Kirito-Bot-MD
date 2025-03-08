3186fe5fdd90731a0d984f3a6f972d5c60214c4f
deylinqff <maycraftminecraft4@gmail.com>
2025-03-08 11:27:39 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,12 +1,12 @@
-handler.help = ['demo *<texto>*'];
-handler.command = ['de', 'openai'];
-handler.tags = ['ai'];
-handler.group = true;
-
 handler.run = async (m, { conn, text, rcanal }) => {
     if (!text) return conn.reply(m.chat, 'Falta el texto.', m, rcanal);
     // Aquí se envía el texto proporcionado por el usuario
     conn.reply(m.chat, `Mensaje recibido: ${text}`, m, rcanal);
 };
+
+handler.help = ['demo *<texto>*'];
+handler.command = ['de', 'openai'];
+handler.tags = ['ai'];
+handler.group = true;
 
 export default handler;
