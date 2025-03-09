b81ef737571e7970db96003eaa2b7d8023f72ef1
deylinqff <maycraftminecraft4@gmail.com>
2025-03-08 22:59:07 -0600

    Actualizar _nable.js

--- plugins/_nable.js
+++ plugins/_nable.js
@@ -335,7 +335,7 @@ const handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, i
 
   chat[type] = isEnable;
 
-  conn.reply(m.chat, `👑 *KIRITO-BOT STATUS*\n━━━━━━━━━━━━━━━━━━━━\n*⚙️ La función *${type}* se ha ${isEnable ? '🟢 ACTIVADO' : '🔴 DESACTIVADO'} ${isAll ? 'para este Bot' : isUser ? 'para este usuario' : 'para este chat'}*\n━━━━━━━━━━━━━━━━━━━━`, m, fake);
+  conn.reply(m.chat, `👑 *KIRITO-BOT STATUS*\n━━━━━━━━━━━━━━━━━━━━\n 👑 La función *${type}* se ha *${isEnable ? ' ACTIVADO' : ' DESACTIVADO'}* ${isAll ? 'para este Bot' : isUser ? 'para este usuario' : 'para este chat'}*\n━━━━━━━━━━━━━━━━━━━━`, m, fake);
 };
 
 handler.help = ['welcome', 'bv', 'bienvenida', 'antiprivado', 'antipriv', 'antiprivate', 'restrict', 'restringir', 'autolevelup', 'autonivel', 'autosticker', 'antibot', 'antibots', 'autoaceptar', 'aceptarauto', 'autorechazar', 'rechazarauto', 'autoresponder', 'autorespond', 'antisubbots', 'antisub', 'antisubot', 'antibot2', 'modoadmin', 'soloadmin', 'autoread', 'autoleer', 'autover', 'antiver', 'antiocultar', 'antiviewonce', 'reaction', 'reaccion', 'emojis', 'nsfw', 'nsfwhot', 'nsfwhorny', 'antispam', 'antiSpam', 'antispamosos', 'antidelete', 'antieliminar', 'jadibotmd', 'modejadibot', 'subbots', 'detect', 'configuraciones', 'avisodegp', 'detect2', 'avisos', 'eventos', 'autosimi', 'simsimi', 'antilink', 'antilink2', 'antitoxic', 'antitoxicos', 'antitraba', 'antitrabas', 'antifake', 'antivirtuales']
