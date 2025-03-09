172b8ccdc8cc5a0c03ea698b1f6638b472cf02e7
deylinqff <maycraftminecraft4@gmail.com>
2025-03-08 22:47:55 -0600

    Actualizar _nable.js

--- plugins/_nable.js
+++ plugins/_nable.js
@@ -15,8 +15,14 @@ const handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, i
     isEnable = false;
 } else {
     const estado = isEnable ? '✓ Activado' : '✗ Desactivado';
-    return conn.reply(m.chat, `「✦」Un administrador puede activar o desactivar el *${command}* utilizando:\n\n> ✐ *${usedPrefix}${command} on* para activar.\n> ✐ *${usedPrefix}${command} off* para desactivar.\n\n✧ Estado actual » *${estado}*`, m);
-  }
+     return conn.reply(m.chat, `👑 *KIRITO-BOT CONTROL*  
+╭──────────────────────────╮  
+│ *🔥 Un administrador puede activar o desactivar el *${command}* utilizando:*  
+│  
+│ *${usedPrefix}${command} on* – 🟢 Activar  
+│ *${usedPrefix}${command} off* – 🔴 Desactivar  
+╰──────────────────────────╯  
+⚡ *Estado actual:* ${estado}`, m, fake)
 
   switch (type) {
     case 'welcome':
