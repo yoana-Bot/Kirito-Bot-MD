c39b1851dc18b0d02bb004c1fcd01c8fa195dc25
deylinqff <maycraftminecraft4@gmail.com>
2025-03-09 17:15:49 -0600

    Actualizar descargas-play1.js

--- plugins/descargas-play1.js
+++ plugins/descargas-play1.js
@@ -69,7 +69,7 @@ const ddownr = {
 const handler = async (m, { conn, text, usedPrefix, command }) => {
   try {
     if (!text.trim()) {
-      return conn.reply(m.chat, `❀ Por favor, ingresa el nombre de la música a descargar.`, m);
+      return conn.reply(m.chat, `👑 Por favor, ingresa el nombre de la música a descargar.⚡`, m);
     }
 
     const search = await yts(text);
