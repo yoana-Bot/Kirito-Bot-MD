0658258f707bcc1e8656d8fec0cc5425f1dca440
deylinqff <maycraftminecraft4@gmail.com>
2025-03-09 10:57:05 -0600

    Actualizar tools-southpark.js

--- plugins/tools-southpark.js
+++ plugins/tools-southpark.js
@@ -16,7 +16,7 @@ let response = await fetch(api);
 let json = await response.json();
 let datas = json.data;
 
-let park = `🍭 *Información De:* ${text}\n\n🍬 *Nombre Oficial:* ${datas.officialName}\n🍰 *Organización:* ${datas.memberOf}\n🔖 *Capital:* ${datas.capitalCity}\n🗺️ *Continente:* ${datas.continent}\n👥 *Población:* ${datas.population}\n💬 *Prefijo:* ${datas.callingCode}\n💸 *Moneda:* ${datas.currency}\n📜 *Descripción:* ${datas.description}`;
+let park = `👑 *Información De:* ${text}\n\n⚡ *Nombre Oficial:* ${datas.officialName}\n🍰 *Organización:* ${datas.memberOf}\n🔖 *Capital:* ${datas.capitalCity}\n🗺️ *Continente:* ${datas.continent}\n👥 *Población:* ${datas.population}\n💬 *Prefijo:* ${datas.callingCode}\n💸 *Moneda:* ${datas.currency}\n📜 *Descripción:* ${datas.description}`;
 
 let img = datas.image;
 
