e8c1ade4c3302b5c139236faf2d978784e59f174
deylinqff <maycraftminecraft4@gmail.com>
2025-03-09 15:28:10 -0600

    Actualizar info-ds.js

--- plugins/info-ds.js
+++ plugins/info-ds.js
@@ -4,7 +4,7 @@ import path from 'path'
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, `${emoji} Utiliza este comando directamente en el número principal del Bot.`, m)
+return conn.reply(m.chat, `${emoji} Utiliza este comando directamente en el número principal del Bot.`, m, fake);
 }
 
 let chatId = m.isGroup ? [m.chat, m.sender] : [m.sender]
@@ -23,14 +23,14 @@ break
 }}}
 
 if (filesDeleted === 0) {
-await conn.reply(m.chat, `${emoji2} No se encontró ningún archivo que incluya la ID del chat.`, m)
+await conn.reply(m.chat, `${emoji2} No se encontró ningún archivo que incluya la ID del chat.`, m, fake);
 } else {
-await conn.reply(m.chat, `${emoji2} Se eliminaron ${filesDeleted} archivos de sesión.`, m)
-conn.reply(m.chat, `${emoji} ¡Hola! ¿logras verme?`, m)
+await conn.reply(m.chat, `${emoji2} Se eliminaron ${filesDeleted} archivos de sesión.`, m, fake);
+conn.reply(m.chat, `${emoji} ¡Hola! ¿logras verme?`, m, fake);
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err)
-await conn.reply(m.chat, `${emoji} Hola Soy ${botname} Sigue El Canal y apoyanos porfavor.\n\n> ${channel}`, m)
+await conn.reply(m.chat, `${emoji} Hola Soy ${botname} Sigue El Canal y apoyanos porfavor.\n\n> ${channel}`, m, fake);
 }
 
 }
