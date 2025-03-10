0a97e803f115a8f3c5c6ecbdcd7989a5f5d10629
deylinqff <maycraftminecraft4@gmail.com>
2025-03-09 22:53:04 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -22,7 +22,6 @@ ADR:;;Dirección de tu empresa;;;;
 TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
 NOTE:${about}
 ADR:;;Dirección de tu empresa;;;;
-X-ABADR:ES
 X-ABLabel:Dirección Web
 X-ABLabel:Correo Electrónico
 X-ABLabel:Teléfono de contacto
