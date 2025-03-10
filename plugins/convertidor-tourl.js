4b4f097c30facc3b23eefdfa7801bac169bc6aef
deylinqff <maycraftminecraft4@gmail.com>
2025-03-10 08:59:06 -0600

    Actualizar convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) return conn.reply(m.chat, `${emoji} Por favor, responda a una *Imagen* o *Vídeo.*`, m)
+  if (!mime) return conn.reply(m.chat, `${emoji} Por favor, responda a una *Imagen* o *Vídeo.*`, m, fake);
   await m.react(rwait)
   try {
   let media = await q.download()
@@ -19,7 +19,7 @@ let handler = async (m) => {
       txt += `*» Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
       txt += `> *${dev}*`
 
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, fkontak)
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, fake, fkontak)
 await m.react(done)
 } catch {
 await m.react(error)
