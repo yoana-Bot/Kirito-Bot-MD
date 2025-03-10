0794662366d0823cc4a37dbadc51ca208be997bd
deylinqff <maycraftminecraft4@gmail.com>
2025-03-09 22:36:05 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,11 +1,48 @@
-let handler = async (m, { conn, usedPrefix, command, args }) => {
-       return conn.reply(m.chat, 'Falta el texto.', m, fake);
-    // Aquí se envía el texto proporcionado por el usuario
-    conn.reply(m.chat, `Mensaje recibido: ${text}`, m, rcanal);
-};
+// código creado por Deylin 
+// https://github.com/deylinqff
+// no quites créditos 
 
-handler.help = ['demo *<texto>*'];
-handler.command = ['de', 'openai'];
-handler.tags = ['ai'];
+import PhoneNumber from 'awesome-phonenumber';
+
+async function handler(m, { conn }) { 
+    let numcreador = '526641804242';
+    let ownerJid = numcreador + '@s.whatsapp.net';
+
+    let name = await conn.getName(ownerJid) || 'Deylin'; 
+    let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
+
+    let empresa = 'ᑲrᥲᥡᥲᥒ- Servicios Tecnológicos';
+
+    let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name};;;
+FN:${name}
+ORG:${empresa};
+TITLE:CEO & Fundador
+TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
+EMAIL:correo@empresa.com
+URL:https://www.tuempresa.com
+NOTE:${about}
+ADR:;;Dirección de tu empresa;;;;
+X-ABADR:ES
+X-ABLabel:Dirección Web
+X-ABLabel:Correo Electrónico
+X-ABLabel:Teléfono de contacto
+X-WA-BIZ-NAME:${name}
+X-WA-BIZ-DESCRIPTION:${about}
+END:VCARD`.trim();
+
+    await conn.sendMessage(m.chat, { 
+        contacts: { 
+            displayName: name, 
+            contacts: [{ vcard }]
+        } 
+    }, { quoted: m });
+}
+
+handler.help = ['owner']; 
+handler.tags = ['main']; 
+handler.command = ['owner', 'creator', 'creador2', 'dueño'];
 
 export default handler;
