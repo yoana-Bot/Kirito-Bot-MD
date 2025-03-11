d35f7de35645ec0711361c14da44d2aa1663dcbc
deylinqff <maycraftminecraft4@gmail.com>
2025-03-11 13:15:32 -0600

    Actualizar _balidcode.js

--- plugins/_balidcode.js
+++ plugins/_balidcode.js
@@ -1,26 +1,26 @@
-const handler = async (m, { conn, command, args }) => {
-    await conn.sendMessage(m.chat, { 
-      text: '⚠ *Si estás conectado a otra sesión de sub-bot, por favor te recomiendo que te desconectes o no te conectes a este bot. Si estás conectado a dos, tu cuenta podría ser baneada de WhatsApp y además podrían surgir problemas en el sistema del bot.*',
-      buttons: [
-        {
-          buttonId: 'servicios',
-          buttonText: { displayText: '#code' },
-        },
-        {
-          buttonId: 'contacto',
-          buttonText: { displayText: '#serbot' },
-        },
-      ],
-      footer: '¡KIRITO-BOT-MD!',
-      viewOnce: true,
-    }, { quoted: m });
-  } else {
-    await conn.sendMessage(m.chat, { text: 'Debes incluir `--code` en el comando.' }, { quoted: m });
-  }
+// Creado por Deylin no quites creditos.
+
+
+const handler = async (m, { conn }) => {
+  await conn.sendMessage(m.chat, { 
+    text: '⚠ *Si estás conectado a otra sesión de sub-bot, por favor te recomiendo que te desconectes o no te conectes a este bot. Si estás conectado a dos, tu cuenta podría ser baneada de WhatsApp y además podrían surgir problemas en el sistema del bot.*',
+    buttons: [
+      {
+        buttonId: 'servicios',
+        buttonText: { displayText: '#serbot --code 👑' },
+      },
+      {
+        buttonId: 'contacto',
+        buttonText: { displayText: '#serbot' },
+      },
+       ],
+    footer: '¡MITSURI - KANROJI - BOT!',
+    viewOnce: true,
+  }, { quoted: m });
 };
 
 handler.tags = ['tools'];
-handler.help = ['fun'];
-handler.command = ['cide', 'Code'];
+handler.help = ['webinfo'];
+handler.command = ['code','Code'];
 
 export default handler;
