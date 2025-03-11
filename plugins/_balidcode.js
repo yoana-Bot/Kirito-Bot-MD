670d3273f9d75f0ae0dba1139e7960b3eaa5b75e
deylinqff <maycraftminecraft4@gmail.com>
2025-03-10 21:47:03 -0600

    Actualizar _balidcode.js

--- plugins/_balidcode.js
+++ plugins/_balidcode.js
@@ -18,6 +18,6 @@ const handler = async (m, { conn }) => {
 
 handler.tags = ['tools'];
 handler.help = ['webinfo'];
-handler.command = ['😾','😾'];
+handler.command = ['serbot code','😾'];
 
 export default handler;
