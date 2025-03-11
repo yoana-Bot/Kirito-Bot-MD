06dcb83441758a1008bc8044c2bcfe2019987bf9
deylinqff <maycraftminecraft4@gmail.com>
2025-03-10 20:32:57 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,46 +4,50 @@ import fetch from 'node-fetch'
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
 
-  // Asignamos una URL de imagen
-  let img = 'https://files.catbox.moe/56el7x.jpg'; // Reemplaza con la URL real de la imagen
+  let who = m.messageStubParameters[0]
+  let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/56el7x.jpg')
+  let img = await (await fetch(`${pp}`)).buffer()
 
-  if (chat.welcome && m.messageStubType == 27) {
-    let welcome = ` ┏━━━━━━━━━━━━━━━━┅┈
+  if (chat.welcome) {
+    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+      let bienvenida = `┏━━━━━━━━━━━━━━━━┅┈
 ┃      🄱🄸🄴🄽🅅🄴🄽🄸🄳🄾
 ┣━━━━━━━━━━━━━━━━┅┈
-┃ 𝗨𝘀𝘂𝗮𝗿𝗶𝗼: @${m.messageStubParameters[0].split`@`[0]}
+┃ 𝗨𝘀𝘂𝗮𝗿𝗶𝗼: ${taguser}
 ┃ 
-┃ 𝗚𝗿𝗨𝗽𝗢: ${groupMetadata.subject}
+┃ 𝗚𝗿𝗨𝗽𝗢: ${groupMetadata.subject} 
 ┃
 ┃ 
 ┗━━━━━━━━━━━━━━━━┅┈`
-    await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
-  }
+      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
+    }
 
-  if (chat.welcome && m.messageStubType == 28) {
-    let bye = `┏━━━━━━━━━━━━━━━━┅┈
+    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
+      let bye = `┏━━━━━━━━━━━━━━━━┅┈
 ┃       🄱.    🄰.    🅈.
 ┣━━━━━━━━━━━━━━━━┅┈
-┃ 𝗨𝘀𝘂𝗮𝗿𝗶𝗼: @${m.messageStubParameters[0].split`@`[0]}
+┃ 𝗨𝘀𝘂𝗮𝗿𝗶𝗼: ${taguser}
 ┃ 
-┃ 𝗚𝗿𝗨𝗽𝗢: ${groupMetadata.subject}
+┃ 𝗚𝗿𝗨𝗽𝗢: ${groupMetadata.subject} 
 ┃
 ┃ 
 ┗━━━━━━━━━━━━━━━━┅┈`
-    await conn.sendLuffy(m.chat, packname, textbot, bye, img, img, redes, fkontak)
-  }
+      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
+    }
 
-  if (chat.welcome && m.messageStubType == 32) {
-    let kick = `┏━━━━━━━━━━━━━━━━┅┈
+    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE) { 
+      let kick = `┏━━━━━━━━━━━━━━━━┅┈
 ┃       🄱.    🄰.    🅈.
 ┣━━━━━━━━━━━━━━━━┅┈
-┃ 𝗨𝘀𝘂𝗮𝗿𝗶𝗼: @${m.messageStubParameters[0].split`@`[0]}
+┃ 𝗨𝘀𝘂𝗮𝗿𝗶𝗼: ${taguser}
 ┃ 
-┃ 𝗚𝗿𝗨𝗽𝗢: ${groupMetadata.subject}
+┃ 𝗚𝗿𝗨𝗽𝗢: ${groupMetadata.subject} 
 ┃
 ┃ 
 ┗━━━━━━━━━━━━━━━━┅┈`
-    await conn.sendLuffy(m.chat, packname, textbot, kick, img, img, redes, fkontak)
+      await conn.sendMessage(m.chat, { image: img, caption: kick, mentions: [who] })
+    }
   }
 }
