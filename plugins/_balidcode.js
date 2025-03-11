ea8b87c799ecf60209e6cb10814fe0b3e8b8e3f3
deylinqff <maycraftminecraft4@gmail.com>
2025-03-11 13:12:47 -0600

    Actualizar _balidcode.js

--- plugins/_balidcode.js
+++ plugins/_balidcode.js
@@ -1,5 +1,4 @@
 const handler = async (m, { conn, command, args }) => {
-  if (args.includes('--code')) {
     await conn.sendMessage(m.chat, { 
       text: '⚠ *Si estás conectado a otra sesión de sub-bot, por favor te recomiendo que te desconectes o no te conectes a este bot. Si estás conectado a dos, tu cuenta podría ser baneada de WhatsApp y además podrían surgir problemas en el sistema del bot.*',
       buttons: [
