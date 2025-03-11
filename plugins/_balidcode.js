7023ee833e6ce9386ff231c891aff9b92c4afea3
deylinqff <maycraftminecraft4@gmail.com>
2025-03-10 21:50:59 -0600

    Actualizar _balidcode.js

--- plugins/_balidcode.js
+++ plugins/_balidcode.js
@@ -1,23 +1,27 @@
-const handler = async (m, { conn }) => {
-  await conn.sendMessage(m.chat, { 
-    text: '⚠ *Si estás conectado a otra sesión de sub-bot, por favor te recomiendo que te desconectes o no te conectes a este bot. Si estás conectado a dos, tu cuenta podría ser baneada de WhatsApp y además podrían surgir problemas en el sistema del bot.*',
-    buttons: [
-      {
-        buttonId: 'servicios',
-        buttonText: { displayText: '#code' },
-      },
-      {
-        buttonId: 'contacto',
-        buttonText: { displayText: '#serbot' },
-      },
-       ],
-    footer: '¡KIRITO-BOT-MD!',
-    viewOnce: true,
-  }, { quoted: m });
+const handler = async (m, { conn, command, args }) => {
+  if (args.includes('--code')) {
+    await conn.sendMessage(m.chat, { 
+      text: '⚠ *Si estás conectado a otra sesión de sub-bot, por favor te recomiendo que te desconectes o no te conectes a este bot. Si estás conectado a dos, tu cuenta podría ser baneada de WhatsApp y además podrían surgir problemas en el sistema del bot.*',
+      buttons: [
+        {
+          buttonId: 'servicios',
+          buttonText: { displayText: '#code' },
+        },
+        {
+          buttonId: 'contacto',
+          buttonText: { displayText: '#serbot' },
+        },
+      ],
+      footer: '¡KIRITO-BOT-MD!',
+      viewOnce: true,
+    }, { quoted: m });
+  } else {
+    await conn.sendMessage(m.chat, { text: 'Debes incluir `--code` en el comando.' }, { quoted: m });
+  }
 };
 
 handler.tags = ['tools'];
-handler.help = ['webinfo'];
-handler.command = ['serbotcode','😾'];
+handler.help = ['serbot --code'];
+handler.command = ['serbot'];
 
 export default handler;
