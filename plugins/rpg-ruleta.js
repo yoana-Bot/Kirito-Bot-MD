5a1af4b5b51017358eeade23f7780c28531f3d9d
deylinqff <maycraftminecraft4@gmail.com>
2025-03-12 13:17:57 -0600

    Actualizar rpg-ruleta.js

--- plugins/rpg-ruleta.js
+++ plugins/rpg-ruleta.js
@@ -13,10 +13,10 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
 
   cooldowns[m.sender] = Date.now()
 
-  if (!text) return conn.reply(m.chat, `${emoji} Debes ingresar una cantidad de *💸 ${moneda}* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m)
+  if (!text) return conn.reply(m.chat, `${emoji} Debes ingresar una cantidad de *🔥 ${moneda}* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m)
 
   let args = text.trim().split(" ")
-  if (args.length !== 2) return conn.reply(m.chat, `${emoji2} Formato incorrecto. Debes ingresar una cantidad de *💸 ${moneda}* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m)
+  if (args.length !== 2) return conn.reply(m.chat, `${emoji2} Formato incorrecto. Debes ingresar una cantidad de *🔥 ${moneda}* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m)
 
   let coin = parseInt(args[0])
   let color = args[1].toLowerCase()
@@ -29,7 +29,7 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
 
   if (coin > users.coin) return conn.reply(m.chat, `${emoji2} No tienes suficientes ${moneda} para realizar esa apuesta.`, m)
 
-  await conn.reply(m.chat, `${emoji} Apostaste ${coin} *💸 ${moneda}* al color ${color}. Espera *⏱ 10 segundos* para conocer el resultado.`, m)
+  await conn.reply(m.chat, `${emoji} Apostaste ${coin} *🔥 ${moneda}* al color ${color}. Espera *⏱ 10 segundos* para conocer el resultado.`, m)
 
   setTimeout(() => {
     let result = Math.random()
@@ -43,10 +43,10 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
 
     if (win) {
       users.coin += coin
-      conn.reply(m.chat, `${emoji} ¡Ganaste! Obtuviste ${coin} *💸 ${moneda}*. Total: ${users.coin} *💸 ${moneda}*.`, m)
+      conn.reply(m.chat, `${emoji} ¡Ganaste! Obtuviste ${coin} *🔥 ${moneda}*. Total: ${users.coin} *🔥 ${moneda}*.`, m)
     } else {
       users.coin -= coin
-      conn.reply(m.chat, `${emoji2} Perdiste. Se restaron ${coin} *💸 ${moneda}*. Total: ${users.coin} *💸 ${moneda}*.`, m)
+      conn.reply(m.chat, `${emoji2} Perdiste. Se restaron ${coin} *🔥 ${moneda}*. Total: ${users.coin} *🔥 ${moneda}*.`, m)
     }
 
 
