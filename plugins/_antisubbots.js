7ce28b749b6d31ab2f47716a65fe8bcb44c4ca8a
deylinqff <maycraftminecraft4@gmail.com>
2025-03-11 19:53:10 -0600

    Actualizar _antisubbots.js

--- plugins/_antisubbots.js
+++ plugins/_antisubbots.js
@@ -17,7 +17,7 @@ export async function before(m, { participants, conn }) {
 
           if (isBotPresent) {
                 setTimeout(async () => {
-                    await conn.reply(m.chat, `《✧》En este grupo está el bot principal, el cual me saldré para no hacer spam.`, m)
+                    await conn.reply(m.chat, `╭━〔 ⚔ 𝐊𝐈𝐑𝐈𝐓𝐎-𝐁𝐎𝐓 ⚔ 〕━╮\n┃ En este grupo ya está el bot principal.\n┃ Me retiraré para evitar spam.\n╰━━━━━━━━━━━━━━━━━━━╯`, m, fake);
                     await this.groupLeave(m.chat)
                 }, 5000)// 5 segundos
             }
