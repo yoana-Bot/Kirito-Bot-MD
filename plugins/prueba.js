5b72742e102c205a5f7023ca9647133d85ecded9
deylinqff <maycraftminecraft4@gmail.com>
2025-03-11 20:56:18 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -21,6 +21,6 @@ const handler = async (m, { conn }) => {
 
 handler.tags = ['tools'];
 handler.help = ['webinfo'];
-handler.command = ['x','X'];
+handler.command = ['Xd','Xd'];
 
 export default handler;
