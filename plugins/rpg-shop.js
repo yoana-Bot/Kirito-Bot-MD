4d1fa7180523b48b54259c40a8b9845f46cae68b
deylinqff <maycraftminecraft4@gmail.com>
2025-03-12 13:11:38 -0600

    Actualizar rpg-shop.js

--- plugins/rpg-shop.js
+++ plugins/rpg-shop.js
@@ -15,7 +15,7 @@ const handler = async (m, {conn, command, args}) => {
 ║│ *Gastado* : -${xppercoin * count} XP
 ║╰──────────────┄
 ╚═══════⩽✰⩾═══════╝`, m);
-  } else conn.reply(m.chat, `${emoji2} Lo siento, no tienes suficiente *XP* para comprar *${count}* ${moneda} 💸`, m);
+  } else conn.reply(m.chat, `${emoji2} Lo siento, no tienes suficiente *XP* para comprar *${count}* ${moneda} 🔥`, m);
 };
 handler.help = ['Buy', 'Buyall'];
 handler.tags = ['economy'];
