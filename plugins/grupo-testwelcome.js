88cadbf422d423e9e884fdafdefcb9a664ba17d5
deylinqff <maycraftminecraft4@gmail.com>
2025-03-13 16:22:17 -0600

    Actualizar grupo-testwelcome.js

--- plugins/grupo-testwelcome.js
+++ plugins/grupo-testwelcome.js
@@ -1,20 +1,19 @@
 import { WAMessageStubType } from '@whiskeysockets/baileys';
 import fetch from 'node-fetch';
 
-global.dfail = (type, m, usedPrefix, command, conn) => {
+let handler = async (m, { conn, usedPrefix, command, text }) => {
     if (!db.data.chats[m.chat].welcome && m.isGroup) {
-        return m.reply(`${emoji} Para usar este comando debe activar las Bienvenidas con *#welcome*`, m, fake);
+    return m.reply(`${emoji} Para usar este comando debe activar las Bienvenidas con *#welcome*`);
     }
-
     let chat = global.db.data.chats[m.chat];
+
     let mentions = text.trim();
     let who = mentions ? conn.parseMention(mentions) : [];
-    
-    if (!text) return conn.reply(m.chat, `${emoji} Menciona al usuario con @ para simular la bienvenida.`, m, fake);
+    if (!text) return conn.reply(m.chat, `${emoji} Menciona al usuario con @ para simular la bienvenida.`, m);
 
     let taguser = `@${who[0].split('@')[0]}`;
     let groupMetadata = await conn.groupMetadata(m.chat);
-    let defaultImage = 'https://files.catbox.moe/xr2m6u.jpg';
+    let defaultImage = 'https://files.catbox.moe/79j62g.jpg';
 
     let img;
     try {
@@ -24,16 +23,16 @@ global.dfail = (type, m, usedPrefix, command, conn) => {
         img = await (await fetch(defaultImage)).buffer();
     }
 
-    let bienvenida = `┏━━━━━━━━━━━━━━━━┅┈
-┃      
+          let bienvenida = `┏━━━━━━━━━━━━━━━━┅┈
+┃      🄱🄸🄴🄽🅅🄴🄽🄸🄳🄾
 ┣━━━━━━━━━━━━━━━━┅┈
 ┃ 𝗨𝘀𝘂𝗮𝗿𝗶𝗼: ${taguser}
 ┃ 
 ┃ 𝗚𝗿𝗨𝗽𝗢: ${groupMetadata.subject} 
 ┃
-┗━━━━━━━━━━━━━━━━┅┈`;
-
-    await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: who }, { quoted: fake });
+┃ 
+┗━━━━━━━━━━━━━━━━┅┈`, m, fake);
+    await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: who });
 };
 
 handler.help = ['testwelcome @user'];
@@ -42,4 +41,4 @@ handler.command = ['welcome1'];
 handler.admin = true;
 handler.group = true;
 
-export default handler; de 
+export default handler;
