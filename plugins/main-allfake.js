69d2b11e59e67fe726ab6547083576a8d97f221f
deylinqff <maycraftminecraft4@gmail.com>
2025-03-13 13:21:44 -0600

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -61,9 +61,10 @@ global.msm = '⚠︎'
 //Emojis determinado de kirito Bot
 global.emoji = '👑'
 global.emoji2 = '👑'
-global.emoji3 = '👑'
+global.emoji3 = '🔥'
 global.emoji4 = '👑'
-global.emoji5 = '👑'
+global.emoji5 = '🔥'
+global.emoji5 = '⚠️'
 global.emojis = [emoji, emoji2, emoji3, emoji4].getRandom()
 
 //mensaje en espera
