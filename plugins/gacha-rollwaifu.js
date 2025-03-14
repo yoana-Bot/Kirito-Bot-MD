cf967019a68e233e2ace3fdd7aec06fad79436fd
deylinqff <maycraftminecraft4@gmail.com>
2025-03-14 16:13:15 -0600

    Actualizar gacha-rollwaifu.js

--- plugins/gacha-rollwaifu.js
+++ plugins/gacha-rollwaifu.js
@@ -10,7 +10,7 @@ async function loadCharacters() {
         const data = await fs.readFile(charactersFilePath, 'utf-8');
         return JSON.parse(data);
     } catch (error) {
-        throw new Error('❀ No se pudo cargar el archivo characters.json.');
+        throw new Error('🔥 No se pudo cargar el archivo characters.json.');
     }
 }
 
@@ -18,7 +18,7 @@ async function saveCharacters(characters) {
     try {
         await fs.writeFile(charactersFilePath, JSON.stringify(characters, null, 2), 'utf-8');
     } catch (error) {
-        throw new Error('❀ No se pudo guardar el archivo characters.json.');
+        throw new Error('🔥 No se pudo guardar el archivo characters.json.');
     }
 }
 
@@ -35,7 +35,7 @@ async function saveHarem(harem) {
     try {
         await fs.writeFile(haremFilePath, JSON.stringify(harem, null, 2), 'utf-8');
     } catch (error) {
-        throw new Error('❀ No se pudo guardar el archivo harem.json.');
+        throw new Error('🔥 No se pudo guardar el archivo harem.json.');
     }
 }
 
@@ -47,7 +47,7 @@ let handler = async (m, { conn }) => {
         const remainingTime = Math.ceil((cooldowns[userId] - now) / 1000);
         const minutes = Math.floor(remainingTime / 60);
         const seconds = remainingTime % 60;
-        return await conn.reply(m.chat, `《✧》Debes esperar *${minutes} minutos y ${seconds} segundos* para usar *#rw* de nuevo.`, m);
+        return await conn.reply(m.chat, `👑 Debes esperar *${minutes} minutos y ${seconds} segundos* para usar *#rw* de nuevo.`, m);
     }
 
     try {
@@ -61,7 +61,7 @@ let handler = async (m, { conn }) => {
             ? `Reclamado por @${randomCharacter.user.split('@')[0]}` 
             : 'Libre';
 
-        const message = `❀ Nombre » *${randomCharacter.name}*
+        const message = `✰ Nombre » *${randomCharacter.name}*
 ✰ Género » *${randomCharacter.gender}*
 ✰ Valor » *${randomCharacter.value}*
 ✰ Estado » ${statusMessage}
