98a2daa63da7b930a41bcaabb1e0898866e97a06
deylinqff <maycraftminecraft4@gmail.com>
2025-03-15 18:36:02 -0600

    Actualizar owner-addimgmenu.js

--- plugins/owner-addimgmenu.js
+++ plugins/owner-addimgmenu.js
@@ -1,32 +1,20 @@
-import { promises as fs } from 'fs'
-import { join } from 'path'
-
-const mediaFile = join(process.cwd(), 'menuMedia.json')
-
-let handler = async (m, { conn }) => {
-  if (!m.quoted) return conn.reply(m.chat, '❎ Responde a una imagen o video.', m)
-
-  let mime = m.quoted.mimetype || ''
-  let isImage = mime.includes('image')
-  let isVideo = mime.includes('video')
-
-  if (!isImage && !isVideo) return conn.reply(m.chat, '❎ Solo puedes usar imágenes o videos.', m)
-
-  let media = await m.quoted.download()
-  let fileType = isImage ? 'jpg' : 'mp4'
-  let fileName = `menuMedia.${fileType}`
-  let filePath = join(process.cwd(), fileName)
-
-  await fs.writeFile(filePath, media)
-
-  let data = { url: `./${fileName}`, type: isImage ? 'image' : 'video' }
-  await fs.writeFile(mediaFile, JSON.stringify(data), 'utf-8')
-
-  global.banner = data.url
-
-  conn.reply(m.chat, `✅ Se ha guardado la nueva ${isImage ? '📷 Imagen' : '🎥 Video'} para el menú.`, m)
+await conn.sendMessage(m.chat, global.banner.includes('.mp4') || global.banner.includes('.webm') ? {
+        caption: txt,
+        contextInfo: { mentionedJid: [userId] },
+        video: { url: banner },
+        gifPlayback: true
+    } : {
+        text: txt,
+        contextInfo: {
+            mentionedJid: [userId],
+            externalAdReply: {
+                title: botname,
+                body: dev,
+                thumbnailUrl: banner,
+                mediaType: 1,
+                showAdAttribution: true,
+                renderLargerThumbnail: true
+            }
+        }
+    }, { quoted: m });
 }
-
-handler.command = ['Addimagemenu', 'delowner']
-handler.rowner = true;
-export default handler;
