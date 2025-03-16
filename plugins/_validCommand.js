deeda72f2ab63ac68367fd3b65b9ff255a92236f
deylinqff <maycraftminecraft4@gmail.com>
2025-03-16 14:41:33 -0600

    Actualizar _validCommand.js

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -8,7 +8,7 @@ export async function before(m) {
 
   const validCommand = (command, plugins) => {
     for (let plugin of Object.values(plugins)) {
-      if (plugin.command && (Array.isArray(plugin.command) ? plugin.command : [plugin.command]).includes(command)) {
+     if (plugin.command && (Array.isArray(plugin.command) ? plugin.command : [plugin.command]).includes(command)) {
         return true;
       }
     }
@@ -23,10 +23,10 @@ export async function before(m) {
       user.commands = 0;
     }
     user.commands += 1;
+   // await conn.sendPresenceUpdate('composing', m.chat);
   } else {
-    const comando = m.text.trim().split(' ')[0];
-    await conn.sendMessage(m.chat, {
-      text: `〘👑〙 𝑬𝒍 𝒄𝒐𝒎𝒂𝒏𝒅𝒐 『 *${comando}* 』 𝒏𝒐 𝒆𝒙𝒊𝒔𝒕𝒆.\n𝑷𝒂𝒓𝒂 𝒗𝒆𝒓 𝒍𝒂 𝒍𝒊𝒔𝒕𝒂 𝒅𝒆 𝒄𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝒖𝒔𝒂:
-*/Menu*`,
+   const comando = m.text.trim().split(' ')[0];
+   await m.reply(`〘👑〙 𝑬𝒍 𝒄𝒐𝒎𝒂𝒏𝒅𝒐 『 *${comando}* 』 𝒏𝒐 𝒆𝒙𝒊𝒔𝒕𝒆.\n𝑷𝒂𝒓𝒂 𝒗𝒆𝒓 𝒍𝒂 𝒍𝒊𝒔𝒕𝒂 𝒅𝒆 𝒄𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝒖𝒔𝒂:
+*/Menu*`);
   }
 }
