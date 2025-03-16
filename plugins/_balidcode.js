690094f1e054fc956062463d61741a8b3d9ef227
deylinqff <maycraftminecraft4@gmail.com>
2025-03-16 14:57:07 -0600

    Actualizar _balidcode.js

--- plugins/_balidcode.js
+++ plugins/_balidcode.js
@@ -1,26 +1,14 @@
-// Creado por Deylin no quites creditos.
-
+// Creado por Deylin, no quites créditos.
 
 const handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, { 
-    text: '⚠ *Si estás conectado a otra sesión de sub-bot, por favor te recomiendo que te desconectes o no te conectes a este bot. Si estás conectado a dos, tu cuenta podría ser baneada de WhatsApp y además podrían surgir problemas en el sistema del bot.*',
-    buttons: [
-      {
-        buttonId: 'servicios',
-        buttonText: { displayText: '#serbot --code 👑' },
-      },
-      {
-        buttonId: '#serbot',
-        buttonText: { displayText: '°serbot ⚡' },
-      },
-       ],
-    footer: '👑 Kirito-Bot MD 👑',
+    text: '⚠ *Si estás conectado a otra sesión de sub-bot, por favor te recomiendo que te desconectes o no te conectes a este bot. Si estás conectado a dos, tu cuenta podría ser baneada de WhatsApp y además podrían surgir problemas en el sistema del bot.*\n\n*/serbot --code*\n\n*/serbot*',
     viewOnce: true,
   }, { quoted: m });
 };
 
 handler.tags = ['tools'];
 handler.help = ['webinfo'];
-handler.command = ['code','Code'];
+handler.command = ['code', 'Code'];
 
 export default handler;
