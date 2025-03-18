bd2618ea31aa957bd4ea965cd9a76d06858b3828
deylinqff <maycraftminecraft4@gmail.com>
2025-03-17 23:11:30 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -41,15 +41,15 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 //let require = createRequire(megu)
 let { say } = cfonts
 
-console.log(chalk.bold.redBright(`\n✰ Iniciando Yuki-Suou-Bot ✰\n`))
+console.log(chalk.bold.redBright(`\n✰ Iniciando Kirito-Bot MD ✰\n`))
 
-say('YukiBot-MD', {
+say('kirito-Bot-MD', {
 font: 'block',
 align: 'center',
 colors: ['magentaBright']
 })
 
-say(`Developed By • The-King-Destroy`, {
+say(`Developed By • Deylin`, {
 font: 'console',
 align: 'center',
 colors: ['blueBright']
