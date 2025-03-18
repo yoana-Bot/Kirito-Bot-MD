1de50784ea32011d1b24630ae050466da5426a21
deylinqff <maycraftminecraft4@gmail.com>
2025-03-17 23:13:45 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ if (opcion == '1' || methodCodeQR) {
 console.log(chalk.bold.yellow(`\n❐ ESCANEA EL CÓDIGO QR EXPIRA EN 45 SEGUNDOS`))}
 }
 if (connection == 'open') {
-console.log(chalk.bold.green('\n❀ YukiBot-MD Conectada con éxito ❀'))
+console.log(chalk.bold.green('\n👑 Kirito-Bot-MD Conectada con éxito 👑'))
 }
 let reason = new Boom(lastDisconnect?.error)?.output?.statusCode
 if (connection === 'close') {
