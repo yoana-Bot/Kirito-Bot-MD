d24f61356529810382b9c2157d24ec0325fbfb4f
deylinqff <maycraftminecraft4@gmail.com>
2025-03-17 23:31:54 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
-import './settings.js'
+import './config.js'
 import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
 import cfonts from 'cfonts'
