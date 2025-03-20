8466a932776c7695c754af4134d82f09a74360f0
deylinqff <maycraftminecraft4@gmail.com>
2025-03-20 11:59:09 -0600

    Actualizar rg-levelup.js

--- plugins/rg-levelup.js
+++ plugins/rg-levelup.js
@@ -2,29 +2,26 @@ import { canLevelUp, xpRange } from '../lib/levelling.js'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
-  let img = await (await fetch(`https://files.catbox.moe/9q2avs.jpg`)).buffer()
-  let name = conn.getName(m.sender)
-  let user = global.db.data.users[m.sender]
-  if (!canLevelUp(user.level, user.exp, global.multiplier)) {
-    let { min, xp, max } = xpRange(user.level, global.multiplier)
-    let txt = `👤 *Nombre* : ${name}\n\n`
-    txt += `⭐ *Nivel* : ${user.level}\n`
-    txt += `✨ *Experiencia* : ${user.exp - min} / ${xp}\n\n`
-    txt += `💫 No es suficiente XP *${max - user.exp}* ¡De nuevo! ✨`
-    await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, { quoted: m, fake: fake })
-  }
-  let before = user.level * 1
-  while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-  if (before !== user.level) {
-    let txt = `🎊 F E L I C I T A C I O N E S 🎊\n\n` 
-    txt += `*${before}* ➔ *${user.level}* [ ${user.role} ]\n\n`
-    txt += `• ⭐ *Nivel anterior* : ${before}\n`
-    txt += `• 🌟 *Nuevos niveles* : ${user.level}\n`
-    txt += `• 📅 *Fecha* : ${new Date().toLocaleString('id-ID')}\n\n`
-    txt += `${emoji} *Nota:* Cuanto más a interactúes con el Bot, mayor será tu nivel.`
-    await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, { quoted: m })
-  }
-}
+let img = await (await fetch(`https://files.catbox.moe/9q2avs.jpg`)).buffer()
+let name = conn.getName(m.sender)
+let user = global.db.data.users[m.sender]
+if (!canLevelUp(user.level, user.exp, global.multiplier)) {
+let { min, xp, max } = xpRange(user.level, global.multiplier)
+let txt = `👤 *Nombre* : ${name}\n\n`
+txt += `⭐ *Nivel* : ${user.level}\n`
+txt += `✨ *Experiencia* : ${user.exp - min} / ${xp}\n\n`
+txt += `💫 No es suficiente XP *${max - user.exp}* ¡De nuevo! ✨`
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)}
+let before = user.level * 1
+while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+if (before !== user.level) {
+let txt = `🎊 F E L I C I T A C I O N E S 🎊\n\n` 
+txt += `*${before}* ➔ *${user.level}* [ ${user.role} ]\n\n`
+txt += `• ⭐ *Nivel anterior* : ${before}\n`
+txt += `• 🌟 *Nuevos niveles* : ${user.level}\n`
+txt += `• 📅 *Fecha* : ${new Date().toLocaleString('id-ID')}\n\n`
+txt += `${emoji} *Nota:* Cuanto más a interactúes con el Bot, mayor será tu nivel.`
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)}}
 
 handler.help = ['levelup']
 handler.tags = ['rpg']
