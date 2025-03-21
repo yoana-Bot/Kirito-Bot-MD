02e01c194b004ce919ccfa2ea0c12270754ecc6c
deylinqff <maycraftminecraft4@gmail.com>
2025-03-20 22:46:25 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -387,27 +387,27 @@ Hola *@${userId.split('@')[0]}* soy *${botname}*
 > © kirito-Bot by Deylin
   `.trim();
 
-  await conn.sendMessage(m.chat, { 
-    text: txt,
-    contextInfo: {
-        mentionedJid: [m.sender, userId],
-        isForwarded: true,
-        forwardedNewsletterMessageInfo: {
-            newsletterJid: channelRD.id,
-            newsletterName: channelRD.name,
-            serverMessageId: -1,
-        },
-        forwardingScore: 999,
-        externalAdReply: {
-            title: botname,
-            body: textbot,
-            thumbnailUrl: banner, // Asegúrate de que esta URL sea válida y accesible
-            mediaType: 1,
-            showAdAttribution: true,
-            renderLargerThumbnail: true,
-        },
-    },
-}, { quoted: m });
+    await conn.sendMessage(m.chat, { 
+      text: txt,
+      contextInfo: {
+          mentionedJid: [m.sender, userId],
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: channelRD.id,
+              newsletterName: channelRD.name,
+              serverMessageId: -1,
+          },
+          forwardingScore: 999,
+          externalAdReply: {
+              title: botname,
+              body: textbot,
+              thumbnailUrl: banner,
+              mediaType: 1,
+              showAdAttribution: true,
+              renderLargerThumbnail: true,
+          },
+      },
+  }, { quoted: m });
 
 };
 
