6e33cc5f41dcda8f305e02ce1f4ef1acdbebbde2
deylinqff <maycraftminecraft4@gmail.com>
2025-03-21 23:41:25 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,42 +1,63 @@
-//código creado por Deylin 
+// Código creado por Deylin 
 // https://github.com/deylinqff
-// no quites créditos 
+// No quites créditos 
 
 import PhoneNumber from 'awesome-phonenumber';
 
 async function handler(m, { conn }) { 
-    let numcreador = '50488198573';
-    let ownerJid = numcreador + '@s.whatsapp.net';
-
-    let name = await conn.getName(ownerJid) || 'Deylin'; 
-    let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
-
+    let numCreador = '50488198573';
+    let ownerJid = numCreador + '@s.whatsapp.net';
+    let nameCreador = await conn.getName(ownerJid) || 'Deylin'; 
+    let aboutCreador = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
     let empresa = 'Deylin - Servicios Tecnológicos';
 
-    let vcard = `
+    let numBot = conn.user.jid.split('@')[0]; 
+    let botJid = numBot + '@s.whatsapp.net';
+    let nameBot = await conn.getName(botJid) || 'Kirito-Bot';
+    let aboutBot = (await conn.fetchStatus(botJid).catch(() => {}))?.status || 'Bot Oficial de Deylin';
+
+    let vcardCreador = `
 BEGIN:VCARD
 VERSION:3.0
-N:;${name};;;
-FN:${name}
+N:;${nameCreador};;;
+FN:${nameCreador}
 ORG:${empresa};
 TITLE:CEO & Fundador
-TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
+TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
 EMAIL:correo@empresa.com
 URL:https://www.tuempresa.com
-NOTE:${about}
+NOTE:${aboutCreador}
 ADR:;;Dirección de tu empresa;;;;
 X-ABADR:ES
 X-ABLabel:Dirección Web
 X-ABLabel:Correo Electrónico
 X-ABLabel:Teléfono de contacto
-X-WA-BIZ-NAME:${name}
-X-WA-BIZ-DESCRIPTION:${about}
+X-WA-BIZ-NAME:${nameCreador}
+X-WA-BIZ-DESCRIPTION:${aboutCreador}
+END:VCARD`.trim();
+
+    let vcardBot = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${nameBot};;;
+FN:${nameBot}
+TITLE:Asistente Virtual
+TEL;waid=${numBot}:${new PhoneNumber('+' + numBot).getNumber('international')}
+EMAIL:bot@tuempresa.com
+URL:https://www.tuempresa.com
+NOTE:${aboutBot}
+X-ABADR:ES
+X-ABLabel:Dirección Web
+X-ABLabel:Correo Electrónico
+X-ABLabel:Teléfono del Bot
+X-WA-BIZ-NAME:${nameBot}
+X-WA-BIZ-DESCRIPTION:${aboutBot}
 END:VCARD`.trim();
 
     await conn.sendMessage(m.chat, { 
         contacts: { 
-            displayName: name, 
-            contacts: [{ vcard }]
+            displayName: 'Deylin & Kirito-Bot', 
+            contacts: [{ vcard: vcardCreador }, { vcard: vcardBot }]
         } 
     }, { quoted: m });
 }
