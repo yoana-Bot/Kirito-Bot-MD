e84983701648887f06f433cf40f7f5f6399a2450
deylinqff <maycraftminecraft4@gmail.com>
2025-03-22 10:29:10 -0600

    Actualizar prueba2.js

---
+++ plugins/prueba2.js
@@ -0,0 +1,14 @@
+// Creado por Deylin, no quites créditos.
+
+const handler = async (m, { conn }) => {
+  await conn.sendMessage(m.chat, { 
+    text: '⚠︎ *Esta función se encuentra en mantenimiento*',
+    viewOnce: true,
+  }, { quoted: m });
+};
+
+handler.tags = ['tools'];
+handler.help = ['webinfo'];
+handler.command = ['spotify', 'Spotifi'];
+
+export default handler;
