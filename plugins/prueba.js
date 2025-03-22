dda1a67ce32ab16e9d82e9a87fbac775bb8cf319
deylinqff <maycraftminecraft4@gmail.com>
2025-03-22 13:44:27 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,10 +1,10 @@
 // Creado por Deylin, no quites créditos.
 
-const handler = async (m, { conn }) => {
+const handler = async (m, { conn, fake }) => {
   await conn.sendMessage(m.chat, { 
     text: '⚠︎ *El comando se encuentra temporalmente fuera de servicio debido a un exceso de solicitudes. Por favor, inténtalo nuevamente más tarde.*',
     viewOnce: true,
-  }, { quoted: m });
+  }, { quoted: m, fake });
 };
 
 handler.tags = ['tools'];
