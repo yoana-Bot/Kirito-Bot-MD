f9dd71648376088391b4a343d4d3ceb3643b8381
Brayan Martínez <lourdesagueda93@gmail.com>
2025-03-21 23:51:27 -0700

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn }) => {
   let name = await conn.getName(who);
 
   await sendContactArray(conn, m.chat, [
-    [`${suittag}`, `Deylin`, namecreador, `❀ No Hacer Spam`, correo, `⊹˚• Venezuela •˚⊹`, md, bio],
+    [`${suittag}`, `Deylin`, name, `❀ No Hacer Spam`, correo, `⊹˚• Venezuela •˚⊹`, md, bio],
     [`${conn.user.jid.split('@')[0]}`, `✦ Es Un Bot`, packname, dev, correo, `Sabra Deylin 🫏`, channel, biobot]
   ], m);
 }
