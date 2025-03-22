77298fae7a4c7f6b27cc3302a8002434505ccea5
deylinqff <maycraftminecraft4@gmail.com>
2025-03-22 00:24:48 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,48 +1,63 @@
-// Código creado por Deylin
+// Código creado por Deylin 
 // https://github.com/deylinqff
-// No quites créditos
+// No quites créditos 
 
 import PhoneNumber from 'awesome-phonenumber';
 
-async function handler(m, { conn }) {
-  const numCreador = '50488198573';
-  const ownerJid = numCreador + '@s.whatsapp.net';
+async function handler(m, { conn }) { 
 
-  const name = await conn.getName(ownerJid) || 'Deylin';
-  const about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
-  const empresa = 'Deylin - Servicios Tecnológicos';
+    let numBot= '50488198573';
+    let ownerJid = numBot + '@s.whatsapp.net';
+    let nameBot = await conn.getName(ownerJid) || 'Kirito-Bot-MD'; 
+    let aboutBot = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
 
-  const vcard = `
+
+    let numCreador = conn.user.jid.split('@')[0]; 
+    let CreadorJid = numCreador + '@s.whatsapp.net';
+    let nameCreador = 'Deylin';
+    let aboutCreador = (await conn.fetchStatus(CreadorJid).catch(() => {}))?.status || 'Bot de asistencia personalizada';
+
+
+    let vcardCreador = `
 BEGIN:VCARD
 VERSION:3.0
-N:;${name};;;
-FN:${name}
+N:;${nameCreador};;;
+FN:${nameCreador}
 ORG:${empresa};
 TITLE:CEO & Fundador
 TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
-EMAIL:correo@empresa.com
-URL:https://www.tuempresa.com
-NOTE:${about}
-ADR:;;Dirección de tu empresa;;;;
-X-ABADR:ES
-X-ABLabel:Dirección Web
-X-ABLabel:Correo Electrónico
-X-ABLabel:Teléfono de contacto
-X-WA-BIZ-NAME:${name}
-X-WA-BIZ-DESCRIPTION:${about}
-END:VCARD
-  `.trim();
+EMAIL:deylibaquedano801@gmail.com
+URL:https://kirito-md.vercel.app/
+NOTE:Perfil oficial de ${nameCreador}. ${aboutCreador}
+X-ABLabel:Contacto Directo
+END:VCARD`.trim();
 
-  // Envío del contacto
-  await conn.sendMessage(
-    m.chat,
-    { contacts: { displayName: name, contacts: [{ vcard }] } },
-    { quoted: m }
-  );
+
+    let vcardBot = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${nameBot};;;
+FN:${nameBot}
+ORG: kirito - by Deylin;
+TITLE:Creador para Asistencia Personalizadas
+TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
+EMAIL:contacto@lusetebot.com
+URL:https://lusetebot.com
+NOTE:Un bot único y diferente, creado para ofrecer asistencia personalizada. ${aboutBot}
+X-ABLabel:Soporte Técnico
+END:VCARD`.trim();
+
+
+    await conn.sendMessage(m.chat, { 
+        contacts: { 
+            displayName: 'Deylin & Lusete Bot', 
+            contacts: [{ vcard: vcardBot }, { vcard: vcardCreador}]
+        } 
+    }, { quoted: m });
 }
 
-handler.help = ['owner'];
-handler.tags = ['main'];
+handler.help = ['owner']; 
+handler.tags = ['main']; 
 handler.command = ['owner', 'creator', 'creador', 'dueño'];
 
 export default handler;
