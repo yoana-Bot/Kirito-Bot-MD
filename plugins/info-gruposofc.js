6e6c5e58d1fcdd9b74f78b736defe0e4c74c6ef3
deylinqff <maycraftminecraft4@gmail.com>
2025-03-21 18:58:33 -0600

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -2,24 +2,24 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
 
-let grupos = `╭━━《《 *GRUPOS OFICIALES* 》》━━╮
+let grupos = `╭━━《 *GRUPOS OFICIALES* 》━━╮
 
-✧ *Únete a nuestra comunidad:*  
+✧ *Únete a nuestro grupo:*  
 ➥ *${namegrupo}*  
 ⌁ ${gp1}  
 
-━═━═━═━═━═━═━═━═━═━═━═━
+━═━═━═━═━═━═━═━═━═━═━
 
 ⚠ *¿Enlace inválido? Usa el alternativo:*  
 ➥ *${namechannel}*  
 ⌁ ${channel}  
 
-━═━═━═━═━═━═━═━═━═━═━═━
+━═━═━═━═━═━═━═━═━═━═━
 
 ☉ *Desarrollador:*  
 ${dev}  
 
-╰━═━═━═━═━═━═━═━═━━═━═━╯`
+╰━═━═━═━═━═━═━═━═━━═━╯`
 
 await conn.sendFile(m.chat, catalogo, "grupos.jpg", grupos, m)
 await m.react(emojis)
