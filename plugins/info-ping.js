589fa9eb9bfbf67970200f28f0a9a7b5bb1b31df
deylinqff <maycraftminecraft4@gmail.com>
2025-03-23 16:05:09 -0600

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -8,9 +8,7 @@ let handler = async (m, { conn }) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
 
-          conn.reply(m.chat, `let message = `> ╭───〔 *¡Pong!* 〕───╮
-> │ ⏱️ *Tiempo:* ${latensi.toFixed(4)} ms
-> ╰─────────────────╯
+          conn.reply(m.chat, `> ╭───〔 *¡Pong!* 〕───╮\n> │ 👑 *Tiempo:* ${latensi.toFixed(4)} ms\n> ╰────────────────────╯
 `;`, m);
             });
 }
