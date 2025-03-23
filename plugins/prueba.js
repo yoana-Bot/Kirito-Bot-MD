1b81f46ea9b848a28171e01eb3c6e3058862569c
deylinqff <maycraftminecraft4@gmail.com>
2025-03-23 16:23:09 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,11 +1,9 @@
 global.dfail = (type, m, usedPrefix, command, conn) => {
-
-let l = "https://files.catbox.moe/56el7x.jpg"
-
-let tex = `hola`, m);
-
-comando = 'tex'
-ejecut 'tex', accion 'l'
+    let l = "https://files.catbox.moe/56el7x.jpg";
+    let tex = "hola";
+    
+    conn.sendMessage(m.chat, { text: tex, image: { url: l } }, { quoted: m });
+};
 
 handler.command = ['l', 'tex'];
 handler.tags = ['downloader'];
