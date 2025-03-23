ed3db5ab2c2ac68754805f1eeb0327ca4c1aa1b4
deylinqff <maycraftminecraft4@gmail.com>
2025-03-23 16:21:48 -0600

    Actualizar prueba.js

---
+++ plugins/prueba.js
@@ -0,0 +1,11 @@
+global.dfail = (type, m, usedPrefix, command, conn) => {
+
+let l = "https://files.catbox.moe/56el7x.jpg"
+
+let tex = `hola`, m);
+
+comando = 'tex'
+ejecut 'tex', accion 'l'
+
+handler.command = ['l', 'tex'];
+handler.tags = ['downloader'];
