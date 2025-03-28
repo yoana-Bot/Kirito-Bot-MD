66907f9a6b325ae5c9c802021fc317b1b9c7b621
deylinqff <maycraftminecraft4@gmail.com>
2025-03-27 21:57:29 -0600

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -27,63 +27,42 @@ return res.data
 console.log(`Error : ${e}`)
 }}
 
+let pp = ''
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+//let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/327f6ad853cb4f405aa80.jpg')
+
 //creador y otros
 global.creador = 'Wa.me/50488198573'
+global.botreal = `${(conn.user.jid == global.conn.user.jid ? 'Oficial' : 'Sub-Bot')}`
 global.ofcbot = `${conn.user.jid.split('@')[0]}`
 global.asistencia = 'Wa.me/50488198573'
-global.namechannel = 'ᴋɪʀɪᴛᴏ - ʙᴏᴛ • ᴜᴘᴅᴀᴛᴇs ☆'
-global.namechannel2 = 'ᴋɪʀɪᴛᴏ - ʙᴏᴛ • ᴀᴠɪsᴏs ☆'
-global.namegrupo = '✦ ᴋɪʀɪᴛᴏ ʙᴏᴛ • ᥆𝖿іᥴіᥲᥣ ☆'
-global.namecomu = '✦ ᴋɪʀɪᴛᴏ ʙᴏᴛ • ᥴ᥆mᥙᥒі𝗍ᥡ ☆'
-global.listo = '☆ *Aquí tienes ฅ՞•ﻌ•՞ฅ*'
-global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
-
-//Ids channel
-global.canalIdM = ["120363365444927738@newsletter", "120363365444927738@newsletter"]
-global.canalNombreM = ["ᴋɪʀɪᴛᴏ - ʙᴏᴛ • ᴜᴘᴅᴀᴛᴇs ☆", "ᴋɪʀɪᴛᴏ - ʙᴏᴛ • ᴀᴠɪsᴏs ☆"]
-global.channelRD = await getRandomChannel()
-
-//fechas
-global.d = new Date(new Date + 3600000)
-global.locale = 'es'
-global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
-global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'})
-global.mes = d.toLocaleDateString('es', {month: 'long'})
-global.año = d.toLocaleDateString('es', {year: 'numeric'})
-global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
 
 //Reacciones De Comandos.!
 global.rwait = '🕒'
 global.done = '✅'
 global.error = '✖️'
-global.msm = '⚠︎'
 
-//Emojis determinado de ᴋɪʀɪᴛᴏ Bot
-global.emoji = '👑'
-global.emoji2 = '👑'
-global.emoji3 = '👑'
-global.emoji4 = '👑'
-global.emoji5 = '👑'
-global.emojis = [emoji, emoji2, emoji3, emoji4].getRandom()
+//Emojis determinado de CrowBot
+global.emoji = '❛‿˂̵✧'
+global.emoji2 = '（＾∀＾●）ﾉｼ'
+global.emoji3 = 'ฅ°ㅅ°ฅ'
+global.emoji4 = 'ฅ՞•ﻌ•՞ฅ'
+global.emoji5 = '˙Ⱉ˙ฅ'
+global.emojis = [emoji, emoji2, emoji3, emoji4, emoji5].getRandom()
 
 //mensaje en espera
-global.wait = '❍ Espera un momento, soy lento...';
-global.waitt = '❍ Espera un momento, soy lento...';
-global.waittt = '❍ Espera un momento, soy lento...';
-global.waitttt = '❍ Espera un momento, soy lento...';
+global.wait = 'ฅ՞•ﻌ•՞ฅ *𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...*';
 
 //Enlaces
-var canal = 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m'  
-let canal2 = 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m'
-var git = 'https://github.com/deylinqff'
 var github = 'https://github.com/deylinqff/Kirito-Bot-MD' 
-let correo = 'deylibaquedano801@gmail.com'
+var web = 'https://kirito-md.vercel.app/' 
+let instagram = 'https://www.instagram.com/'
 
-global.redes = [canal, canal2, git, github, correo].getRandom()
+global.redes = [github, web, instagram].getRandom()
 
 //Imagen
 let category = "imagen"
-const db = './src/database/db.json'
+const db = './media/database/db.json'
 const db_ = JSON.parse(fs.readFileSync(db))
 const random = Math.floor(Math.random() * db_.links[category].length)
 const randomlink = db_.links[category][random]
@@ -96,25 +75,28 @@ var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'L
 global.saludo = hour;
 
 //tags
-global.nombre = m.pushName || 'Anónimo'
+global.nombre = conn.getName(m.sender)
 global.taguser = '@' + m.sender.split("@s.whatsapp.net")
 var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
 
-global.packsticker = `°.______________.°\n✦ Usuario: ${nombre}\n☆ Bot: ${botname}\n✦ Fecha: ${fecha}\nⴵ Hora: ${tiempo}`;
-global.packsticker2 = `\n°._______________.°\n\n${dev}`
+global.authsticker = `┊🤖 Bot Ofc\n┊↳ ${wm}\n\n┊👑 Usuario\n┊↳ @${conn.getName(m.sender)}\n`;
+
+global.packsticker = `┊ 👑 kirito-Bot MD\n↳https://kirito-md.vercel.app/\n\n┊ 🔥 ɪɴғᴏʀᴍᴀᴄɪᴏɴ\n↳https://github.com/deylinqff/Kirito-Bot-MD`
 
 //Fakes
-global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `6285600793871-1614953337@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `${nombre}`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${nombre},;;;\nFN:${nombre},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': null, thumbnail: null,sendEphemeral: true}}}
+global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=50557865603:50557865603\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, newsletterName: channelRD.name, serverMessageId: -1 }
+global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363377595441592@newsletter', newsletterName: "👑 . ˚ Cһᥲᥒᥒᥱᥣ 〢𝚔𝚒𝚛𝚒𝚝𝚘 - 𝙱𝚘𝚝 𝙼𝙳 ☆˒˒", serverMessageId: -1 }
 }}, { quoted: m }
 
-global.icono = [
-'https://tinyurl.com/285a5ejf',
-].getRandom()
+//Id Channel
+global.idchannel = '120363381910502266@newsletter'
+global.canalIdM = ["120363368073378190@newsletter", "120363387375075395@newsletter"]
+global.canalNombreM = ["✯ 🄺🄸🅁🄸🅃🄾 - 🅲🅷🅰🅽🅽🅴🅻 ✯", "⏤͟͟͞͞𝐤𝐢𝐫𝐢𝐭𝐨 - 𝐁𝐨𝐭『𝐌𝐃』"]
+global.channelRD = await getRandomChannel()
 
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
+global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: textbot, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icons, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
 
 export default handler
 
@@ -127,4 +109,4 @@ let randomIndex = Math.floor(Math.random() * canalIdM.length)
 let id = canalIdM[randomIndex]
 let name = canalNombreM[randomIndex]
 return { id, name }
-}
+}         
