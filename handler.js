15838231c4a247ba0dc9107f35e43853d0219b41
deylinqff <maycraftminecraft4@gmail.com>
2025-03-27 23:09:34 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -607,7 +607,7 @@ global.dfail = (type, m, conn) => {
     restrict: `*⚡ Esta función está bloqueada. Fin de la historia.*`
   }[type];
 
-  if (msg) return conn.reply(m.chat, msg, m, fkontak).then(_ => m.react('✖️'));}
+  if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'));}
 
 let file = global.__filename(import.meta.url, true)
 watchFile(file, async () => {
