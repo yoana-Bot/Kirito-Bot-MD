e2c31456d6ede5803bb3eb4114f8984201203461
deylinqff <maycraftminecraft4@gmail.com>
2025-04-02 17:23:41 -0600

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -4,32 +4,35 @@ let handler = async (m, { conn }) => {
     let img = './src/catalogo.jpg';
 
     
-    let creador = ['50488198573', 'Deylin 👑', true ]
-
-    lat staff = [
-  [ '59169739411', '*~Iván⁩*', true ],
-  [ '573007796996', '*~𝕄𝕠𝕣𝕒𝕝𝕖𝕤⁩*', true ],
-  [ '50231458537', '*Brayan moscoso*', true ],
-  [ '584245610338', '*jose*', true ],
-  [ '50557865603', '*willzek 🎩*', true ],
-  [ '51928616320', '*manuel*', true ],
-  [ '50557333744', '*Jonathang*', true ],
-  [ '573154062343', '*http*', true ],
-  [ '526671548329', '*~/ty.{LegnaJS}⁩*', true ],
-  [ '526633900512', '*Braya*n⁩ ', true ],
-  [ '51920227615', '*~ai.SEƗSĦƗROṈȺGƗ.il⁩* ',true ]
-]
+    let creador = [['50488198573', 'Deylin 👑']];
 
     
-    let staffMessage = "🌟 *Lista de Staff* 🌟\n\n" +
+    let staff = [
+        ['59169739411', '*~Iván⁩*'],
+        ['573007796996', '*~𝕄𝕠𝕣𝕒𝕝𝕖𝕤⁩*'],
+        ['50231458537', '*Brayan Moscoso*'],
+        ['584245610338', '*José*'],
+        ['50557865603', '*Willzek 🎩*'],
+        ['51928616320', '*Manuel*'],
+        ['50557333744', '*Jonathang*'],
+        ['573154062343', '*http*'],
+        ['526671548329', '*~/ty.{LegnaJS}⁩*'],
+        ['526633900512', '*Brayan*'],
+        ['51920227615', '*~ai.SEƗSĦƗROṈȺGƗ.il⁩*']
+    ];
 
-
-let staffMessage = "*creador*\n\n" +
-        creador.map(([number, name]) => `- ${name}:\n https://wa.me/${number}\n\n`).join('\n');
-let staffMessage = " *colaboradores* \n\n" +
-staff.map(([number, name]) => `- ${name}:\n https://wa.me/${number}\n\n`).join('\n');
-
+    
+    let staffMessage = "🌟 *Lista de Staff* 🌟\n\n";
+    
+    
+    staffMessage += "*👑 Creador 👑*\n";
+    staffMessage += creador.map(([number, name]) => `- ${name}:\n https://wa.me/${number}\n\n`).join('');
+    
    
+    staffMessage += "🎩 *Colaboradores* 🎩\n";
+    staffMessage += staff.map(([number, name]) => `- ${name}:\n https://wa.me/${number}\n\n`).join('');
+
+    
     if (!fs.existsSync(img)) {
         console.error(`Error: La imagen ${img} no existe.`);
         return m.reply("⚠️ Imagen no encontrada.");
