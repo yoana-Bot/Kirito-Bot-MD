692a14c8f6071af63f9a1b37233f3d0b046866b4
deylinqff <maycraftminecraft4@gmail.com>
2025-04-02 20:25:32 -0600

    Actualizar owner-prefix.js

--- plugins/owner-prefix.js
+++ plugins/owner-prefix.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (!text) throw `${emoji} No Se Encontró Ningun Prefijo, Por Favor Escriba Un Prefijo.\n> *Ejemplo: ${usedPrefix + command} !*`;
-  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
+  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!👑#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
  // await m.reply(`*✅️ Prefijo Actualizado Con Éxito, Prefijo Actual: ${text}*`);
   conn.fakeReply(m.chat, `${done} *Prefijo Actualizado Con Éxito, Prefijo Actual: ${text}*`, '0@s.whatsapp.net', '✨ PREFIJO NUEVO ✨')
 };
