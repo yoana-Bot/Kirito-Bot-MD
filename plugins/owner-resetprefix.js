4a992b35eec06794fbbf15b132b4793912001c15
deylinqff <maycraftminecraft4@gmail.com>
2025-04-02 20:25:07 -0600

    Actualizar owner-resetprefix.js

--- plugins/owner-resetprefix.js
+++ plugins/owner-resetprefix.js
@@ -1,5 +1,5 @@
 const handler = async (m, {conn}) => {
-  global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
+  global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()👑[\]^$+*?.\-\^]/g, '\\$&') + ']');
   //await m.reply(`✅️ *Prefijo Restablecido Con Éxito!*`);
   conn.fakeReply(m.chat, `${emoji} *Prefijo Restablecido Con Éxito!*`, '0@s.whatsapp.net', '✨ PREFIJO RESTABLECIDO ✨')
 };
