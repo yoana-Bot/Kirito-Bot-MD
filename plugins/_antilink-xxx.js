fb3cbdf3276b572904605e19e227e4ae9a8daa16
deylinqff <maycraftminecraft4@gmail.com>
2025-04-03 16:09:44 -0600

    Actualizar _antilink-xxx.js

--- plugins/_antilink-xxx.js
+++ plugins/_antilink-xxx.js
@@ -1,8 +1,8 @@
 let adultContentRegex = /(pornhub.com|xvideos.com|xnxx.com|redtube.com|youporn.com|onlyfans.com|sex.com|chaturbate.com|cam4.com|playboy.com|xxx.com|escort|fetish|adult|erotic|nude|hentai|sexchat|camsoda|bongacams|spankbang|rule34|nsfw|18\+|hardcore|taboo|bdsm)/i;
 
 export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, participants }) {
-    if (!m.isGroup) return;  // Solo actúa en grupos
-    if (isAdmin || isOwner || m.fromMe || isROwner) return;  // Excluye admins y propietarios
+    if (!m.isGroup) return;  
+    if (isAdmin || isOwner || m.fromMe || isROwner) return;  
 
     let chat = global.db.data.chats[m.chat];
     let delet = m.key.participant;
