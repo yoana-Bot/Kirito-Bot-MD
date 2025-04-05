bb90f56327f261f4e86cfe092c7b5f4aca70bb05
deylinqff <maycraftminecraft4@gmail.com>
2025-04-05 17:41:06 -0600

    Actualizar sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,18 +1,24 @@
 import { addExif } from '../lib/sticker.js';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!m.quoted) return m.reply(`${emoji} Por favor, responde a un sticker con el comando *${usedPrefix + command}* seguido del nuevo nombre.\nEjemplo: *${usedPrefix + command} Nuevo Nombre*`, m, rcanal);
+  if (!m.quoted) {
+    return m.reply(`${emoji} Por favor, responde a un sticker con el comando *${usedPrefix + command}* seguido del nuevo nombre.\nEjemplo: *${usedPrefix + command} Nuevo Nombre*`, m, rcanal);
+  }
 
   const sticker = await m.quoted.download();
-  if (!sticker) return m.reply(`${emoji2} No se pudo descargar el sticker.`);
+  if (!sticker) {
+    return m.reply(`${emoji2} No se pudo descargar el sticker.`);
+  }
 
   const textoParts = text.split(/[\u2022|]/).map(part => part.trim());
   const texto1 = textoParts[0] || `✦ ${botname}`;
-  const texto2 = textoParts[1] || `${nombre}`
+  const texto2 = textoParts[1] || `${nombre}`;
 
   const exif = await addExif(sticker, texto1, texto2);
 
-  await conn.sendMessage(m.chat, { sticker: exif }, { quoted: fkontak });
+  const chatId = String(m.chat);
+
+  await conn.sendMessage(chatId, { sticker: exif }, { quoted: fkontak });
 };
 
 handler.help = ['wm'];
