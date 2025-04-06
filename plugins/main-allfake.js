3574c9661a3bcb9cb35e847eeb98399c364b3917
deylinqff <maycraftminecraft4@gmail.com>
2025-04-06 17:12:14 -0600

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -102,7 +102,11 @@ global.channelRD = await getRandomChannel()
 
 global.icono = [ 
 'https://i.postimg.cc/RFdNynN5/IMG-20250315-WA0122.jpg',
-'https://i.postimg.cc/RFdNynN5/IMG-20250315-WA0122.jpg'
+'https://i.postimg.cc/RFdNynN5/IMG-20250315-WA0122.jpg',
+'https://i.postimg.cc/RFdNynN5/IMG-20250315-WA0122.jpg',
+'https://i.postimg.cc/3JjzVC0N/IMG-20250318-WA0969.jpg',
+'https://i.postimg.cc/3JjzVC0N/IMG-20250318-WA0969.jpg',
+'https://i.postimg.cc/3JjzVC0N/IMG-20250318-WA0969.jpg'
 ].getRandom()
 
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: textbot, body: '👑҉Kirito- Bot MDᚐ', mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
