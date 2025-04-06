a76e87c165320d3d4dd0f994d37ca3da3edd6c3a
deylinqff <maycraftminecraft4@gmail.com>
2025-04-05 18:05:01 -0600

    Actualizar jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -65,11 +65,11 @@ function convertirMsADiasHorasMinutosSegundos(ms) {
 }
 
 const message = users.map((v, i) => 
-`╭───[ Sub-Bot #${i + 1} ]────
-│ Nombre : ${v.user.name || 'Sub-Bot'}
-│ Enlace : wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}estado
-│ Online : ${v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : 'Desconocido'}
-╰─────────────────────────`).join('\n\n');
+`> ╭───[ Sub-Bot #${i + 1} ]────
+> │ Nombre : ${v.user.name || 'Sub-Bot'}
+> │ Enlace : wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}estado
+> │ Online : ${v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : 'Desconocido'}
+> ╰────────────────────`).join('\n\n');
 
 const responseMessage = `KIRITO-BOT\nSub-Bots activos: ${users.length}\n\n${message || 'No hay sub-bots conectados.'}`.trim();
 
