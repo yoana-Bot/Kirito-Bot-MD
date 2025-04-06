9816b9504ee7e6cda8e85e30493547b29dc4e1a3
deylinqff <maycraftminecraft4@gmail.com>
2025-04-05 21:41:01 -0600

    Actualizar game-adivinabandera.js

--- plugins/game-adivinabandera.js
+++ plugins/game-adivinabandera.js
@@ -70,14 +70,14 @@ function elegirBanderaAleatoria() {
 
 let handler = async (m, { conn }) => {
   if (juegoBanderas.has(m.sender)) {
-    return conn.reply(m.chat, "Ya tienes un juego en curso. ¡Responde primero!", m, rcanal)
+    return conn.reply(m.chat, "Ya tienes un juego en curso. ¡Responde primero!", m)
   }
 
   const seleccionada = elegirBanderaAleatoria()
   juegoBanderas.set(m.sender, { pais: seleccionada.pais.toLowerCase(), intentos: 2 })
 
   let text = `*🎌 Adivina la bandera:*\n\n» ${seleccionada.emoji}\n\n*Responde con el nombre del país.*\nTienes 2 corazones ❤️❤️`
-  conn.reply(m.chat, text, m, rcanal);
+  conn.reply(m.chat, text, m)
 }
 
 handler.before = async (m, { conn }) => {
@@ -88,14 +88,14 @@ handler.before = async (m, { conn }) => {
   const respuesta = m.text.trim().toLowerCase()
   if (respuesta === juego.pais) {
     juegoBanderas.delete(m.sender)
-    return conn.reply(m.chat, `¡Correcto! Adivinaste la bandera de *${juego.pais.charAt(0).toUpperCase() + juego.pais.slice(1)}*\n\n*Has ganado:* ${expGanada} Exp.🥳`, m, rcanal);
+    return conn.reply(m.chat, `¡Correcto! Adivinaste la bandera de *${juego.pais.charAt(0).toUpperCase() + juego.pais.slice(1)}*\n\n*Has ganado:* ${expGanada} Exp.🥳`, m)
   } else {
     juego.intentos--
     if (juego.intentos <= 0) {
       juegoBanderas.delete(m.sender)
-      return conn.reply(m.chat, `❌ Perdiste. La respuesta correcta era *${juego.pais.charAt(0).toUpperCase() + juego.pais.slice(1)}*`, m, rcanal);
+      return conn.reply(m.chat, `❌ Perdiste. La respuesta correcta era *${juego.pais.charAt(0).toUpperCase() + juego.pais.slice(1)}*`, m)
     } else {
-      return conn.reply(m.chat, `❌ Incorrecto. Te quedan ${juego.intentos} corazón(es) ❤️`, m, rcanal);
+      return conn.reply(m.chat, `❌ Incorrecto. Te quedan ${juego.intentos} corazón(es) ❤️`, m)
     }
   }
 }
