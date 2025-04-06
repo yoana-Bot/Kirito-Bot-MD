d117b4785fa89b7bacefc550dd8ce51618213e0d
deylinqff <maycraftminecraft4@gmail.com>
2025-04-05 20:55:26 -0600

    Actualizar game-adivinabandera.js

--- plugins/game-adivinabandera.js
+++ plugins/game-adivinabandera.js
@@ -70,31 +70,32 @@ function elegirBanderaAleatoria() {
 
 let handler = async (m, { conn }) => {
   if (juegoBanderas.has(m.sender)) {
-    return conn.reply(m.chat, "Ya tienes un juego en curso. ¡Responde primero!", m)
+    return conn.reply(m.chat, "Ya tienes un juego en curso. ¡Responde primero!", m, rcanal)
   }
 
   const seleccionada = elegirBanderaAleatoria()
   juegoBanderas.set(m.sender, { pais: seleccionada.pais.toLowerCase(), intentos: 2 })
 
   let text = `*🎌 Adivina la bandera:*\n\n» ${seleccionada.emoji}\n\n*Responde con el nombre del país.*\nTienes 2 corazones ❤️❤️`
-  conn.reply(m.chat, text, m)
+  conn.reply(m.chat, text, m, rcanal)
 }
 
 handler.before = async (m, { conn }) => {
   const juego = juegoBanderas.get(m.sender)
   if (!juego) return
+        global.db.data.users[sender].exp += expGanada;
 
   const respuesta = m.text.trim().toLowerCase()
   if (respuesta === juego.pais) {
     juegoBanderas.delete(m.sender)
-    return conn.reply(m.chat, `¡Correcto! Adivinaste la bandera de *${juego.pais.charAt(0).toUpperCase() + juego.pais.slice(1)}* 🥳`, m)
+    return conn.reply(m.chat, `¡Correcto! Adivinaste la bandera de *${juego.pais.charAt(0).toUpperCase() + juego.pais.slice(1)}*\n\n*Has ganado:* ${expGanada} Exp.🥳`, m, rcanal)
   } else {
     juego.intentos--
     if (juego.intentos <= 0) {
       juegoBanderas.delete(m.sender)
-      return conn.reply(m.chat, `❌ Perdiste. La respuesta correcta era *${juego.pais.charAt(0).toUpperCase() + juego.pais.slice(1)}*`, m)
+      return conn.reply(m.chat, `❌ Perdiste. La respuesta correcta era *${juego.pais.charAt(0).toUpperCase() + juego.pais.slice(1)}*`, m, rcanal)
     } else {
-      return conn.reply(m.chat, `❌ Incorrecto. Te quedan ${juego.intentos} corazón(es) ❤️`, m)
+      return conn.reply(m.chat, `❌ Incorrecto. Te quedan ${juego.intentos} corazón(es) ❤️`, m, rcanal)
     }
   }
 }
