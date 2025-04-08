89288db92d8751797c43cd32e97361225bc353b1
deylinqff <maycraftminecraft4@gmail.com>
2025-04-08 10:13:05 -0600

    Actualizar ai-dalle.js

--- plugins/ai-dalle.js
+++ plugins/ai-dalle.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const handler = async (m, { conn, args }) => {
     if (!args[0]) {
-        await conn.reply(m.chat, `${emoji} Por favor, proporciona una descripción para generar la imagen.`, m);
+        await conn.reply(m.chat, `${emoji} Por favor, proporciona una descripción para generar la imagen.`, m, rcanal);
         return;
     }
 
@@ -12,14 +12,14 @@ const handler = async (m, { conn, args }) => {
     const apiUrl = `https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${prompt}`;
 
     try {
-        conn.reply(m.chat, `${emoji2} Espere un momento...`, m)
+        conn.reply(m.chat, `${emoji2} Espere un momento...`, m, rcanal);
 
         const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
 
         await conn.sendMessage(m.chat, { image: Buffer.from(response.data) }, { quoted: m });
     } catch (error) {
         console.error('Error al generar la imagen:', error);
-        await conn.reply(m.chat, `${msm} No se pudo generar la imagen, intenta nuevamente mas tarde.`, m);
+        await conn.reply(m.chat, `${msm} No se pudo generar la imagen, intenta nuevamente mas tarde.`, m, rcanal);
     }
 };
 
