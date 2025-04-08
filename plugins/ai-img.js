7756385a72cf561ca08f1459f76ecf89a7be4ade
deylinqff <maycraftminecraft4@gmail.com>
2025-04-08 10:37:39 -0600

    Actualizar ai-img.js

--- plugins/ai-img.js
+++ plugins/ai-img.js
@@ -1,3 +1,4 @@
+
 import axios from 'axios';
 
 const handler = async (m, { conn, args }) => {
@@ -10,19 +11,15 @@ const handler = async (m, { conn, args }) => {
     const apiUrl = `https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${prompt}`;
 
     try {
-        await conn.reply(m.chat, `${emoji2} Espere un momento...`, m, rcanal);
+        conn.reply(m.chat, `${emoji2} Espere un momento...`, m, rcanal);
 
         const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
-        const imageBuffer = Buffer.from(response.data);
 
-        await conn.sendMessage(m.chat, {
-            image: imageBuffer,
-            caption: 'Imagen generada'
-        }, { quoted: m, ...rcanal });
-
+let text = ` imagen generada con éxito`
+        await conn.sendMessage(m.chat, { image: Buffer.from(response.data) }, { info: text }, { quoted: fkontak });
     } catch (error) {
         console.error('Error al generar la imagen:', error);
-        await conn.reply(m.chat, `${msm} No se pudo generar la imagen, intenta nuevamente más tarde.`, m, rcanal);
+        await conn.reply(m.chat, `${msm} No se pudo generar la imagen, intenta nuevamente mas tarde.`, m, rcanal);
     }
 };
 
