ceabcd84ae88e7bff63f8c7e73cd5e2b146fdf2c
deylinqff <maycraftminecraft4@gmail.com>
2025-04-10 20:47:56 -0600

    Actualizar _balidcode.js

--- plugins/_balidcode.js
+++ plugins/_balidcode.js
@@ -4,11 +4,11 @@ const handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, { 
     text: ' ⚠ *Si estás conectado a otra sesión de sub-bot, por favor te recomiendo que te desconectes o no te conectes a este bot. Si estás conectado a dos, tu cuenta podría ser baneada de WhatsApp y además podrían surgir problemas en el sistema del bot.*\n\n*/CODE*\n> Vincula con código de 8 dígitos\n*/QR*\n> vincula con código QR',
     viewOnce: true,
-  }, { quoted: m });
+  }, { quoted: fkontak });
 };
 
 handler.tags = ['tools'];
 handler.help = ['webinfo'];
-handler.command = ['serbot'];
+handler.command = ['code', 'qr'];
 
 export default handler;
