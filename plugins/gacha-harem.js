9ce4e8c5ce5e0f5dbb34caec1e3b616fc380703a
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-04-12 09:36:18 -0700

    Actualizar gacha-harem.js

--- plugins/gacha-harem.js
+++ plugins/gacha-harem.js
@@ -8,7 +8,7 @@ async function loadCharacters() {
         const data = await fs.readFile(charactersFilePath, 'utf-8');
         return JSON.parse(data);
     } catch (error) {
-        throw new Error('❀ No se pudo cargar el archivo characters.json.');
+        throw new Error('❀ No se pudo cargar el archivo characters.json.', m,rcanal);
     }
 }
 
@@ -38,7 +38,7 @@ let handler = async (m, { conn, args }) => {
         const userCharacters = characters.filter(character => character.user === userId);
 
         if (userCharacters.length === 0) {
-            await conn.reply(m.chat, '❀ No tiene personajes reclamados en tu harem.', m);
+            await conn.reply(m.chat, '❀ No tiene personajes reclamados en tu harem.', m,rcanal);
             return;
         }
 
@@ -50,7 +50,7 @@ let handler = async (m, { conn, args }) => {
         const endIndex = Math.min(startIndex + charactersPerPage, totalCharacters);
 
         if (page < 1 || page > totalPages) {
-            await conn.reply(m.chat, `❀ Página no válida. Hay un total de *${totalPages}* páginas.`, m);
+            await conn.reply(m.chat, `❀ Página no válida. Hay un total de *${totalPages}* páginas.`, m,rcanal);
             return;
         }
 
@@ -67,7 +67,7 @@ let handler = async (m, { conn, args }) => {
 
         await conn.reply(m.chat, message, m, { mentions: [userId] });
     } catch (error) {
-        await conn.reply(m.chat, `✘ Error al cargar el harem: ${error.message}`, m);
+        await conn.reply(m.chat, `✘ Error al cargar el harem: ${error.message}`, m,rcanal);
     }
 };
 
