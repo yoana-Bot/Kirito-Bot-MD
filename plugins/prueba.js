7e29a99958f0b9c1f69daac86bc2368f58af156a
deylinqff <maycraftminecraft4@gmail.com>
2025-04-13 13:47:57 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -2,32 +2,13 @@ const WAMessageStubType = (await import('@whiskeysockets/baileys')).default;
 
 export async function before(m, { conn }) {
   // Asegurarse que sea un grupo y tenga tipo de mensaje de sistema
-  if (!m.isGroup || m.messageStubType !== 24) return;
+  if (!m.isGroup || m.messageStubType !== WAMessageStubType.GROUP_CHANGE_DESCRIPTION) return;
 
   const dbChat = global.db.data.chats[m.chat];
   if (!dbChat || !dbChat.detect) return;
 
   const sender = `@${m.sender.split('@')[0]}`;
   const newDescription = m.messageStubParameters[0];
-
-  const contactMessage = {
-    key: {
-      participants: "0@s.whatsapp.net",
-      remoteJid: "status@broadcast",
-      fromMe: false,
-      id: "Kirito"
-    },
-    message: {
-      contactMessage: {
-        vcard: `BEGIN:VCARD
-VERSION:3.0
-FN:Kirito-Bot
-TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}
-END:VCARD`
-      }
-    },
-    participant: "0@s.whatsapp.net"
-  };
 
   const messageText = `*${sender}*\n【⚔】 Ha cambiado la descripción del grupo:\n\n${newDescription}`;
 
@@ -36,11 +17,6 @@ END:VCARD`
     {
       text: messageText,
       mentions: [m.sender]
-    },
-    {
-      quoted: contactMessage,
-      ephemeralExpiration: 24 * 60 * 100,
-      disappearingMessagesInChat: 24 * 60 * 100
     }
   );
 }
