1304d4eb7a6519f3d3a659c7d0b9b31c5d40ff73
deylinqff <maycraftminecraft4@gmail.com>
2025-04-13 15:22:04 -0600

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -150,7 +150,7 @@ let secret = await sock.requestPairingCode((m.sender.split`@`[0]))
 secret = secret.match(/.{1,4}/g)?.join("-")
 //if (m.isWABusiness) {
 txtCode = await conn.sendMessage(m.chat, {text : rtx2}, { quoted: fkontak })
-codeBot = await conm.sendMessage(m.chat,{ text : `${secret}` }, { quoted: fkontak })
+codeBot = await conm.sendMessage(m.chat, { text : `codigo\n\n\n${secret}` }, { quoted: fkontak })
 //} else {
 //txtCode = await conn.sendButton(m.chat, rtx2.trim(), wm, null, [], secret, null, m) 
 //}
