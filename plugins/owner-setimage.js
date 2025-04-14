c19e615341fab9c76859dad17ebbf8dd4851c730
deylinqff <maycraftminecraft4@gmail.com>
2025-04-14 13:17:29 -0600

    Actualizar owner-setimage.js

--- plugins/owner-setimage.js
+++ plugins/owner-setimage.js
@@ -1,20 +1,20 @@
 import Jimp from 'jimp';
 
 let handler = async (m, { conn }) => {
-  if (!m.quoted) return conn.reply(m.chat, `${emoji} Por favor, responde a una imagen para cambiar la foto de perfil.`, m);
+  if (!m.quoted) return conn.reply(m.chat, `${emoji} Por favor, responde a una imagen para cambiar la foto de perfil.`, m, rcanal);
 
   try {
     const media = await m.quoted.download();
-    if (!media) return conn.reply(m.chat, `${emoji2} No se pudo obtener la imagen.`, m);
+    if (!media) return conn.reply(m.chat, `${emoji2} No se pudo obtener la imagen.`, m, fake);
 
     const image = await Jimp.read(media);
     const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
 
     await conn.updateProfilePicture(conn.user.jid, buffer);
-    return conn.reply(m.chat, `${emoji} Foto de perfil cambiada con éxito.`, m);
+    return conn.reply(m.chat, `${emoji} Foto de perfil cambiada con éxito.`, m, fake);
   } catch (e) {
     console.error(e);
-    return conn.reply(m.chat, `${msm} Ocurrió un error al intentar cambiar la foto de perfil.`, m);
+    return conn.reply(m.chat, `${msm} Ocurrió un error al intentar cambiar la foto de perfil.`, m, rcanal);
   }
 };
 
