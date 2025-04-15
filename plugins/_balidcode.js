4ef16435b75ff95406b17a4a0049cd607678c0c6
deylinqff <maycraftminecraft4@gmail.com>
2025-04-15 15:17:38 -0600

    Actualizar _balidcode.js

--- plugins/_balidcode.js
+++ plugins/_balidcode.js
@@ -4,19 +4,6 @@ import fetch from 'node-fetch';
 
 const handler = async (m, { conn, text }) => {
   if (!text) {
-    await conn.sendMessage(m.chat, { text: '*🌺 𝑭𝒂𝒍𝒕𝒂 𝒆𝒍 𝒕𝒆𝒙𝒕𝒐 𝒑𝒂𝒓𝒂 𝒄𝒓𝒆𝒂𝒓 𝒍𝒂 𝒊𝒎𝒂𝒈𝒆𝒏✎*' }, { quoted: m });
-    return;
-  }
-
-  m.react('✨');
-  await conn.sendMessage(m.chat, { text: `*🌹 𝒄𝒓𝒆𝒂𝒏𝒅𝒐 𝒊𝒎𝒂𝒈𝒆𝒏 𝒅𝒆 ✎ ${text}*` }, { quoted: m });
-
-  try {
-    const res = await fetch(`https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${encodeURIComponent(text)}`);
-    if (!res.ok) throw new Error();
-
-    const buffer = await res.buffer();
-    m.react('🪄');
     await conn.sendMessage(m.chat, { 
       image: buffer, 
       caption: 'Imagen generada con éxito. Elige una opción:',
