23d7868648d699ac28ee7a9123e189787f801f15
deylinqff <maycraftminecraft4@gmail.com>
2025-04-15 15:39:51 -0600

    Actualizar _balidcode.js

--- plugins/_balidcode.js
+++ plugins/_balidcode.js
@@ -1,32 +1,29 @@
 // Creado por Deylin, no quites créditos.
 
 let handler = async (m, { conn, usedPrefix, command, args }) => {
-    // Definición de los botones
-    const buttons = [
-                    {
-                "name": "cta_copy",
-                "buttonParamsJson": JSON.stringify({
-                "display_text": "Descargar audio! 🎧",
-                "copy_code": `.ytmp3`
-                })
-              },{
-                "name": "cta_copy",
-                "buttonParamsJson": JSON.stringify({
-                "display_text": "Descargar video! 📹",
-                "copy_code": `.ytmp4`
-                })
-              }
-                ];
+    // Array de datos para los botones
+    const buttonData = [
+        { display_text: "/qr", copy_code: "/qr" },
+        { display_text: "/serbot --code", copy_code: "/serbot --code" }
+    ];
+    
+    // Genera los botones de manera simplificada
+    const buttons = buttonData.map(data => ({
+        name: "cta_copy",
+        buttonParamsJson: JSON.stringify(data)
+    }));
     
     // Mensaje con botones
-    const buttonMessage = {
-        text: '⚠ *Si estás conectado a otra sesión de sub-bot, por favor te recomiendo que te desconectes o no te conectes a este bot. Si estás conectado a dos, tu cuenta podría ser baneada de WhatsApp y además podrían surgir problemas en el sistema del bot.*\n\n*/serbot --code*\n> Vincula con código de 8 dígitos\n*/serbot*\n> vincula con código QR',
-        footer: 'Deylin',
-        buttons: buttons,
-        headerType: 1
-    };
-    
-    return conn.sendMessage(m.chat, buttonMessage, { quoted: m });
+    return conn.sendMessage(
+        m.chat,
+        {
+            text: '⚠ *Si estás conectado a otra sesión de sub-bot, por favor te recomiendo que te desconectes o no te conectes a este bot. Si estás conectado a dos, tu cuenta podría ser baneada de WhatsApp y además podrían surgir problemas en el sistema del bot.*\n\n*/serbot --code*\n> Vincula con código de 8 dígitos\n*/serbot*\n> vincula con código QR',
+            footer: 'Deylin',
+            buttons: buttons,
+            headerType: 1
+        },
+        { quoted: m }
+    );
 };
 
 handler.tags = ['tools'];
