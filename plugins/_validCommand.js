af07ce07c3d511ece1e0dc2fa60442f765ab736a
deylinqff <maycraftminecraft4@gmail.com>
2025-04-14 21:44:52 -0600

    Actualizar _validCommand.js

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -1,4 +1,4 @@
-export async function before(m) {
+export async function before(m, { conn }) { 
   if (!m.text || !global.prefix.test(m.text)) {
     return;
   }
@@ -19,23 +19,22 @@ export async function before(m) {
 
   if (command === "bot") {
     return;
-    }
+  }
+
   if (validCommand(command, global.plugins)) {
     let chat = global.db.data.chats[m.chat];
     let user = global.db.data.users[m.sender];
 
     if (chat.isBanned) {
       const avisoDesactivado = `➳✰ 𝐄𝐥 𝐛𝐨𝐭 *${botname}* 𝐞𝐬𝐭𝐚 𝐟𝐮𝐞𝐫𝐚 𝐝𝐞 𝐬𝐞𝐫𝐯𝐢𝐜𝐢𝐨 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨✦.\n\n> ✎ 𝐔𝐧 *𝐚𝐝𝐦𝐢𝐧* 𝐩𝐮𝐞𝐝𝐞 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨 𝐜𝐨𝐧 𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨✦:\n> » *${usedPrefix}𝗸𝗶𝗿𝗶𝘁𝗼 𝗼𝗻*`;
-      await m.reply(avisoDesactivado);
+      await conn.reply(m.chat, avisoDesactivado, m, { quoted: rcanal });
       return;
     }
 
-    if (!user.commands) {
-      user.commands = 0;
-    }
+    if (!user.commands) user.commands = 0;
     user.commands += 1;
   } else {
     const comando = m.text.trim().split(' ')[0];
-    await m.reply(`✎⍰ 𝑬𝒍 𝒄𝒐𝒎𝒂𝒏𝒅𝒐《 *${comando}* 》𝒏𝒐 𝒆𝒙𝒊𝒔𝒕𝒆.\n𝒑𝒆𝒓𝒂 𝒗𝒆𝒓 𝒍𝒂 𝒍𝒊𝒔𝒕𝒂 𝒅𝒆 𝒄𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝒖𝒔𝒂:\n» *#𝗺𝗲𝗻𝘂*`);
+    await conn.reply(m.chat, `✎⍰ 𝑬𝒍 𝒄𝒐𝒎𝒂𝒏𝒅𝒐《 *${comando}* 》𝒏𝒐 𝒆𝒙𝒊𝒔𝒕𝒆.\n𝒑𝒆𝒓𝒂 𝒗𝒆𝒓 𝒍𝒂 𝒍𝒊𝒔𝒕𝒂 𝒅𝒆 𝒄𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝒖𝒔𝒂:\n» *#𝗺𝗲𝗻𝘂*`, m, { quoted: rcanal });
   }
 }
