1fd0c880e652200139f2236715f4a4f870dd67ac
deylinqff <maycraftminecraft4@gmail.com>
2025-04-15 11:03:22 -0600

    Actualizar descargas-play1.js

--- plugins/descargas-play1.js
+++ plugins/descargas-play1.js
@@ -145,6 +145,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 
 handler.command = handler.help = ["play", "play2", "ytmp3", "yta", "ytmp4", "ytv"];
 handler.tags = ["downloader"];
+handler.coin = 5;
 
 export default handler;
 
