239c284bc283a8b111ab8eef0a847015e9ea8a5e
deylinqff <maycraftminecraft4@gmail.com>
2025-04-15 21:43:27 -0600

    Actualizar grupo-link.js

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -1,15 +1,8 @@
-const handler = async (m, { conn, participants, groupMetadata }) => {
-  try {
-    const group = m.chat;
-    const link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group);
-    const mensaje = `\t\t☆ 𝐂𝐨𝐦𝐩𝐚𝐫𝐭𝐞 ☆\n𝗚𝗿𝗨𝗽𝗢: ${groupMetadata.subject}\n\n${link}`;
-    
-    await conn.reply(m.chat, mensaje, m, { detectLink: true });
-  } catch (e) {
-    console.error(e);
-    m.reply('❌ Ocurrió un error al obtener el enlace del grupo.');
-  }
-};
+async function handler(m, { conn, orgs, participants, groupMetadata }) {
+  let group = m.chat;
+  let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group);
+  conn.reply(m.chat, `\t\t☆ 𝐂𝐨𝐦𝐩𝐚𝐫𝐭𝐞 ☆\n𝗚𝗿𝗨𝗽𝗢: ${groupMetadata.subject}\n\n${link}`, m, { detectLink: true });
+}
 
 handler.help = ['link'];
 handler.tags = ['grupo'];
