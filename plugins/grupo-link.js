663335a51d3e34417294108c9ff3b8bc857de839
deylinqff <maycraftminecraft4@gmail.com>
2025-04-15 21:53:37 -0600

    Actualizar grupo-link.js

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -2,7 +2,7 @@ async function handler(m, { conn, orgs, participants, groupMetadata }) {
   let group = m.chat;
   let totalMembers = participants.length;
   let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group);
-  conn.reply(m.chat, `☆ 𝐂𝐨𝐦𝐩𝐚𝐫𝐭𝐞 ☆\n\n𝐆𝐫𝐮𝐩𝐨: ${groupMetadata.subject}\n𝐌𝐢𝐞𝐦𝐛𝐫𝐨𝐬: ${totalMembers}\n\n*link*\n${link}`, m, { detectLink: true });
+  conn.reply(m.chat, `☆ 𝐂𝐨𝐦𝐩𝐚𝐫𝐭𝐞 ☆\n\n𝐆𝐫𝐮𝐩𝐨: ${groupMetadata.subject}\n𝐌𝐢𝐞𝐦𝐛𝐫𝐨𝐬: ${totalMembers}\n\n\n*________________________________*\n*link*\n${link}`, m, { detectLink: true });
 }
 
 handler.help = ['link'];
