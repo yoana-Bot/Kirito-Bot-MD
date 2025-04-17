b5c1b253dcf46b6d9fb61ae5c5b699e2afe29695
deylinqff <maycraftminecraft4@gmail.com>
2025-04-16 21:06:33 -0600

    Actualizar _prueba.js

--- plugins2/_prueba.js
+++ plugins2/_prueba.js
@@ -1,1 +1,12 @@
-h
+// Creado por Deylin, no quites créditos.
+
+let handler = async (m, { conn, usedPrefix, command, args }) => {
+    let userId = m.sender;
+          return conn.reply(m.chat, `Hola *@${userId.split('@')[0]}* ${saludo}*`, m, rcanal)
+};
+
+handler.tags = ['tools'];
+handler.help = ['webinfo'];
+handler.command = ['code', 'ñ'];
+
+export default handler;
