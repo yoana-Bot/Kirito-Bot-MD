df4568f12040f6b8b228174c25d9bbfc2a853ad8
deylinqff <maycraftminecraft4@gmail.com>
2025-04-24 17:12:01 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
-import './config.js'
+import './kirito.core.js'
 import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
 import cfonts from 'cfonts'
