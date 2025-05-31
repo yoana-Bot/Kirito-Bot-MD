0a305ca99852a4b9f233fdcab637de333c4ceddb
BrayanOFC <lourdesagueda93@gmail.com>
2025-05-30 21:49:34 -0700

    Actualizar ai-llama.js

--- plugins/ai-llama.js
+++ plugins/ai-llama.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) return conn.reply(m.chat, `${emoji} Ingrese un texto para hablar con Llama AI.`, m)
     try {
-        let api = await fetch(`https://kirito-md.vercel.app/api/ia-voz?text=${text}`)
+        let api = await fetch(`https://vegeta-md.vercel.app/api/ia-voz?text=${text}`)
         let json = await api.json()
         let responseMessage = json.data;
 
