8a7b62f1dbc76693d6fff77ba6f3ef7adbcf06a6
BrayanOFC <lourdesagueda93@gmail.com>
2025-05-30 21:51:44 -0700

    Actualizar jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -20,13 +20,13 @@ let uniqid = `${who.split`@`[0]}`
 const path = `./${jadi}/${uniqid}`
 
 if (!await fs.existsSync(path)) {
-await conn.sendMessage(m.chat, { text: `Kirito-Bot: no hay sesión activa.\nUsa: ${usedPrefix + command}\nSi tienes ID:\n${usedPrefix + command} (ID)` }, { quoted: m })
+await conn.sendMessage(m.chat, { text: `vegeta-Bot: no hay sesión activa.\nUsa: ${usedPrefix + command}\nSi tienes ID:\n${usedPrefix + command} (ID)` }, { quoted: m })
 return
 }
 if (global.conn.user.jid !== conn.user.jid) {
   return conn.sendMessage(m.chat, {text: `Este comando solo funciona en el bot principal.\nLink: https://wa.me/${global.conn.user.jid.split`@`[0]}?text=${usedPrefix + command}`}, { quoted: m }) 
 } else {
-  await conn.sendMessage(m.chat, { text: `Kirito-Bot: sub-bot desconectado.` }, { quoted: m })
+  await conn.sendMessage(m.chat, { text: `vegeta-Bot: sub-bot desconectado.` }, { quoted: m })
 }
 try {
 fs.rmdir(`./${jadi}/` + uniqid, { recursive: true, force: true })
@@ -40,7 +40,7 @@ case isCommand2:
 if (global.conn.user.jid == conn.user.jid) {
   conn.reply(m.chat, `Solo los sub-bots pueden usar este comando.`, m)
 } else {
-  await conn.reply(m.chat, `Kirito-Bot desactivado.`, m)
+  await conn.reply(m.chat, `vegeta-Bot desactivado.`, m)
   conn.ws.close()
 }  
 break
@@ -71,7 +71,7 @@ const message = users.map((v, i) =>
 > │ Online : ${v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : 'Desconocido'}
 > ╰────────────────────`).join('\n\n');
 
-const responseMessage = `*KIRITO-BOT*\n*Sub-Bots activos*: ${users.length}\n\n${message || 'No hay sub-bots conectados.'}`.trim();
+const responseMessage = `*VEGETA-BOT*\n*Sub-Bots activos*: ${users.length}\n\n${message || 'No hay sub-bots conectados.'}`.trim();
 
 await _envio.sendMessage(m.chat, {text: responseMessage, mentions: _envio.parseMention(responseMessage)}, {quoted: fkontak})
 break   
